985d876a71c3843312acea809c072e8a4d706156
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-04-28 11:25:59 +0200

    Expand and refactor test

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/controller/api/NormsControllerApiTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/controller/api/NormsControllerApiTest.java
@@ -1,5 +1,6 @@
 package de.bund.digitalservice.ris.search.integration.controller.api;
 
+import static de.bund.digitalservice.ris.ZipTestUtils.readZipStream;
 import static de.bund.digitalservice.ris.search.utils.JsonLdUtils.writeJsonLdString;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -26,13 +27,10 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 import org.hamcrest.Matchers;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -184,16 +182,7 @@ class NormsControllerApiTest extends ContainersIntegrationBase {
 
     byte[] zipBytes = result.getResponse().getContentAsByteArray();
     ByteArrayInputStream byteInputStream = new ByteArrayInputStream(zipBytes);
-    ZipInputStream zipInputStream = new ZipInputStream(byteInputStream);
-
-    Map<String, byte[]> files = new HashMap<>();
-
-    ZipEntry entry;
-    while ((entry = zipInputStream.getNextEntry()) != null) {
-      files.put(entry.getName(), zipInputStream.readAllBytes());
-      zipInputStream.closeEntry();
-    }
-    zipInputStream.close();
+    final Map<String, byte[]> files = readZipStream(byteInputStream);
 
     String resourceDirectoryPath =
         getClass()
