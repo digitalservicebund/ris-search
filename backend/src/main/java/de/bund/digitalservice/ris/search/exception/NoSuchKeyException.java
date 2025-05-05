81f2261579718a3fd5d99b5f864aef9add5fd525
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-05-05 14:49:11 +0200

    Change inheritance of NoSuchKeyException

--- backend/src/main/java/de/bund/digitalservice/ris/search/exception/NoSuchKeyException.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/exception/NoSuchKeyException.java
@@ -1,6 +1,6 @@
 package de.bund.digitalservice.ris.search.exception;
 
-public class NoSuchKeyException extends ObjectStoreServiceException {
+public class NoSuchKeyException extends Exception {
   public NoSuchKeyException(String message, Throwable cause) {
     super(message, cause);
   }
