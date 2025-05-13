6ba8e1493986eb15fdee9cc5d90cd933d5f9f58a
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-05-13 17:28:54 +0200

    Adapt header name

--- frontend/src/server/api/[...].ts
+++ frontend/src/server/api/[...].ts
@@ -36,7 +36,7 @@ export default defineEventHandler(async (event): Promise<unknown> => {
     const headers: Record<string, string> = {
       Accept: event.headers.get("Accept") ?? "application/json",
       Authorization: `Bearer ${token}`,
-      "x-resource-base": "PROXY",
+      "get-resources-via": "PROXY",
     };
     return await $fetch.raw(newUrl, {
       headers,
