ba83f94888947c1b567592e9b3e627bd3a0202ff
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-05-22 21:16:02 +0200

    Revert "Debug Keycloak errors"
    This reverts commit 98de17881e634635762e2d6cb51cd361555af03f.

--- frontend/src/server/routes/auth/keycloak/index.get.ts
+++ frontend/src/server/routes/auth/keycloak/index.get.ts
@@ -1,5 +1,16 @@
 import type { H3Event } from "h3";
 import { buildSessionAttributes } from "./keycloakUtils";
+import { authRedirectCookieName } from "@/server/auth";
+
+const getRedirectUrl = (event: H3Event) => {
+  const redirectUrl = getCookie(event, authRedirectCookieName) || "/";
+  deleteCookie(event, authRedirectCookieName);
+
+  if (redirectUrl.startsWith("/")) {
+    return redirectUrl;
+  }
+  return "/";
+};
 
 export default defineOAuthKeycloakEventHandler({
   async onSuccess(event, { user, tokens }) {
@@ -11,17 +22,14 @@ export default defineOAuthKeycloakEventHandler({
       ...buildSessionAttributes(tokens),
     });
 
-    return sendRedirect(event, "/");
+    return sendRedirect(event, getRedirectUrl(event));
   },
-  async onError(event: H3Event, error: Error) {
+  async onError(event: H3Event) {
     const query = getQuery(event);
     // see https://www.keycloak.org/securing-apps/oidc-layers#_oidc-errors
     if (query["error_description"] === "authentication_expired") {
       console.log("Keycloak authentication_expired");
       return sendRedirect(event, "/auth/keycloak");
-    } else {
-      console.error("keycloak error", error);
-      return sendError(event, error, true);
     }
   },
 });
