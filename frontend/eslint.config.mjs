96d5e01e1f0647066782ff3b40c9bc1871f72a40
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-05-26 12:03:26 +0200

    Bump eslint

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -11,6 +11,7 @@ export default createConfigForNuxt(undefined, [
       "vue/multi-word-component-names": "off",
       "vue/no-multiple-template-root": "off",
       "vue/no-v-html": "off",
+      "vue/require-default-prop": "off",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "warn",
