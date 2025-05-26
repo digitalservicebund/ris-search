adb54c364a85625a3d208134cc8b1ada6ce023f6
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-05-26 14:51:15 +0200

    Update logging

--- backend/src/main/java/de/bund/digitalservice/ris/search/service/IndexingState.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/service/IndexingState.java
@@ -2,15 +2,11 @@ package de.bund.digitalservice.ris.search.service;
 
 import java.time.Instant;
 import lombok.With;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 
 @With
 public record IndexingState(
     @Nullable String lastProcessedChangelogFile, String startTime, String lockTime) {
-
-  private static final Logger logger = LogManager.getLogger(IndexingState.class);
 
   public IndexingState() {
     this(null, null, null);
