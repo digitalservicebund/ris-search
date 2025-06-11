a73dbbe40df963cc74d971f958f605ea02f284d0
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-06-11 15:47:34 +0200

    Remove log

--- frontend/src/server/routes/auth/success.get.ts
+++ frontend/src/server/routes/auth/success.get.ts
@@ -4,7 +4,7 @@ import { authRedirectCookieName } from "@/server/auth";
 export default defineEventHandler((event: H3Event) => {
   const redirectUrl = getCookie(event, authRedirectCookieName);
   deleteCookie(event, authRedirectCookieName);
-  console.log({ redirectUrl });
+
   const isValidRedirectUrl = redirectUrl?.startsWith("/");
   if (!isValidRedirectUrl || !redirectUrl) {
     return sendRedirect(event, "/");
