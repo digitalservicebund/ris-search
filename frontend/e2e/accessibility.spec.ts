2e7ccf88e0a733088219040b9f62c6c27f0d6acd
Haytham Mones <haythameyd@gmail.com>
2025-06-13 20:53:27 +0200

    RISDEV-4627 Adding Accessibility e2e Tests
    This PR adds the accessibility tests to all pages of the portal.
    
    They will be skipped for now until we can make tickets for the changes
    and that the changes are targeted one by one.

---
+++ frontend/e2e/accessibility.spec.ts
@@ -0,0 +1,116 @@
+import { expect, test } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
+import { createHtmlReport } from "axe-html-reporter";
+import path from "node:path";
+
+const testPages = [
+  {
+    name: "Home Page",
+    url: "/",
+  },
+  {
+    name: "All Search Results Page",
+    url: "/search",
+  },
+  {
+    name: "Contact Page",
+    url: "/kontakt",
+  },
+  {
+    name: "Imprint Page",
+    url: "/impressum",
+  },
+  {
+    name: "Data Protection Page",
+    url: "/datenschutz",
+  },
+  {
+    name: "Accessibility Page",
+    url: "/barrierefreiheit",
+  },
+  {
+    name: "Cookie Settings Page",
+    url: "/cookie-einstellungen",
+  },
+  {
+    name: "Open Source Page",
+    url: "/opensource",
+  },
+  {
+    name: "User Tests Page",
+    url: "/nutzungstests",
+  },
+  {
+    name: "Norms Search Page",
+    url: "/search?category=N",
+  },
+  {
+    name: "Caselaw Search Page",
+    url: "/search?category=R",
+  },
+  {
+    name: "Article View Page",
+    url: "/norms/eli/bund/bgbl-1/1972/s2459/1999-04-20/4/deu/regelungstext-1/hauptteil-1_abschnitt-1_art-1",
+  },
+];
+test.describe("General Pages Accessibility Tests", () => {
+  testPages.forEach(({ name, url }) => {
+    test(`${name} should not have accessibility issues`, async ({ page }) => {
+      await page.goto(url);
+      await page.waitForLoadState("networkidle");
+      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+      createHtmlReport({
+        results: accessibilityScanResults,
+        options: {
+          outputDir: path.join("e2e", "test-results", "accessibility-results"),
+          reportFileName: `${name}.html`,
+        },
+      });
+      expect(accessibilityScanResults.violations).toEqual([]);
+    });
+  });
+});
+test.describe("View Page Accessibility Tests", () => {
+  test(`Norms page should not have accessibility issues`, async ({ page }) => {
+    await page.goto(
+      "/norms/eli/bund/bgbl-1/2020/s1126/2022-08-04/1/deu/regelungstext-1",
+    );
+    await page.waitForLoadState("networkidle");
+    const mainTabScanResults = await new AxeBuilder({ page }).analyze();
+    createHtmlReport({
+      results: mainTabScanResults,
+      options: {
+        outputDir: path.join("e2e", "test-results", "accessibility-results"),
+        reportFileName: `Norms View Page.html`,
+      },
+    });
+    expect(mainTabScanResults.violations).toEqual([]);
+    await page.getByRole("tab", { name: "Details" }).click();
+    await page.waitForLoadState("networkidle");
+    const detailsTabScanResults = await new AxeBuilder({ page }).analyze();
+    expect(detailsTabScanResults.violations).toEqual([]);
+    await page.getByRole("tab", { name: "Fassungen" }).click();
+    await page.waitForLoadState("networkidle");
+    const versionsTabScanResults = await new AxeBuilder({ page }).analyze();
+    expect(versionsTabScanResults.violations).toEqual([]);
+  });
+  test(`Caselaw page should not have accessibility issues`, async ({
+    page,
+  }) => {
+    await page.goto("/case-law/STRE300770800");
+    await page.waitForLoadState("networkidle");
+    const mainTabScanResults = await new AxeBuilder({ page }).analyze();
+    createHtmlReport({
+      results: mainTabScanResults,
+      options: {
+        outputDir: path.join("e2e", "test-results", "accessibility-results"),
+        reportFileName: `Caselaw View Page.html`,
+      },
+    });
+    expect(mainTabScanResults.violations).toEqual([]);
+    await page.getByRole("tab", { name: "Details" }).click();
+    await page.waitForLoadState("networkidle");
+    const detailsTabScanResults = await new AxeBuilder({ page }).analyze();
+    expect(detailsTabScanResults.violations).toEqual([]);
+  });
+});
