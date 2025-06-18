81049ccc31287c06a0a0ee2ba5db85489e7c8e52
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-06-18 11:23:08 +0200

    set profile on soap config

--- backend/src/main/java/de/bund/digitalservice/ris/search/config/SoapWebServiceConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/config/SoapWebServiceConfig.java
@@ -4,12 +4,14 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 
 @EnableWs
 @Configuration
+@Profile({"prototype", "default"})
 public class SoapWebServiceConfig extends WsConfigurerAdapter {
 
   @Bean
