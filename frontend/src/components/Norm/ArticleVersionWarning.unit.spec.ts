e6fde425d95b6a62690e89b7f43a887b5a3e9195
Haytham Mones <haythameyd@gmail.com>
2025-06-25 22:58:34 +0200

    Removed code duplication in tests

--- frontend/src/components/Norm/ArticleVersionWarning.unit.spec.ts
+++ frontend/src/components/Norm/ArticleVersionWarning.unit.spec.ts
@@ -17,48 +17,33 @@ const formattedDate = (d: string | null) => {
 };
 vi.stubGlobal("formattedDate", formattedDate);
 
+const articleTestData = [
+  {
+    entryIntoForceDate: "1990-01-01",
+    expiryDate: "2000-01-01",
+  },
+  {
+    entryIntoForceDate: "2100-01-01",
+    expiryDate: null,
+  },
+  {
+    entryIntoForceDate: "2000-01-02",
+    expiryDate: null,
+  },
+] as unknown as Article[];
+
 describe("ArticleVersionWarning.vue", () => {
   const inForceVersionLink =
     "/norms/eli/bund/bgbl-1/2000/s100/2000-01-01/1/deu";
-  it("shows warning for historical article", () => {
-    const currentArticle = {
-      entryIntoForceDate: "1990-01-01",
-      expiryDate: "2000-01-01",
-    } as unknown as Article;
-    const wrapper = mount(ArticleVersionWarning, {
-      props: { inForceVersionLink, currentArticle },
-      global: { stubs: ["WarningMessage"] },
+  for (const currentArticle of articleTestData) {
+    it(`shows warning for article with entry date ${currentArticle.entryIntoForceDate} and expiry date ${currentArticle.expiryDate}`, () => {
+      const wrapper = mount(ArticleVersionWarning, {
+        props: { inForceVersionLink, currentArticle },
+        global: { stubs: ["WarningMessage"] },
+      });
+      expect(wrapper.findComponent({ name: "WarningMessage" }).exists()).toBe(
+        true,
+      );
     });
-    expect(wrapper.findComponent({ name: "WarningMessage" }).exists()).toBe(
-      true,
-    );
-  });
-
-  it("shows warning for future article", () => {
-    const currentArticle = {
-      entryIntoForceDate: "2100-01-01",
-      expiryDate: null,
-    } as unknown as Article;
-    const wrapper = mount(ArticleVersionWarning, {
-      props: { inForceVersionLink, currentArticle },
-      global: { stubs: ["WarningMessage"] },
-    });
-    expect(wrapper.findComponent({ name: "WarningMessage" }).exists()).toBe(
-      true,
-    );
-  });
-
-  it("shows warning for inForce article", () => {
-    const currentArticle = {
-      entryIntoForceDate: "2000-01-02",
-      expiryDate: null,
-    } as unknown as Article;
-    const wrapper = mount(ArticleVersionWarning, {
-      props: { inForceVersionLink, currentArticle },
-      global: { stubs: ["WarningMessage"] },
-    });
-    expect(wrapper.findComponent({ name: "WarningMessage" }).exists()).toBe(
-      true,
-    );
-  });
+  }
 });
