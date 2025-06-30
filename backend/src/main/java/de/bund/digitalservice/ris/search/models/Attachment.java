e5570bc12a5f4c3e3538008197af771d4850b993
Carl Gödecken <carl.goedecken@digitalservice.bund.de>
2025-06-30 19:36:24 +0200

    Handle attachment titles

--- backend/src/main/java/de/bund/digitalservice/ris/search/models/Attachment.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/models/Attachment.java
@@ -1,7 +1,12 @@
 package de.bund.digitalservice.ris.search.models;
 
 import lombok.Builder;
+import org.springframework.lang.Nullable;
 
 @Builder
 public record Attachment(
-    String marker, String docTitle, String eId, String textContent, String manifestationEli) {}
+    @Nullable String marker,
+    @Nullable String docTitle,
+    String eId,
+    String textContent,
+    String manifestationEli) {}
