0376b2b0d191d53a8fc1f13ee951439112172267
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-07-07 08:04:47 +0200

    Remove comments from test

--- backend/src/test/java/de/bund/digitalservice/ris/search/unit/mapper/CaseLawEncodingSchemaMapperTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/unit/mapper/CaseLawEncodingSchemaMapperTest.java
@@ -13,33 +13,27 @@ class CaseLawEncodingSchemaMapperTest {
   @Test
   @DisplayName("correctly creates case law encoding schemas")
   void fromDomain() {
-    // Given
     var source =
         CaseLawDocumentationUnit.builder().id("id1").documentNumber("BFRE000087655").build();
 
-    // When
     List<CaseLawEncodingSchema> schemas = CaseLawEncodingSchemaMapper.fromDomain(source);
 
-    // Then
     assertEquals(3, schemas.size(), "Should have three schemas");
 
     String base = "/v1/case-law/" + source.documentNumber();
 
-    // HTML Schema
     CaseLawEncodingSchema htmlSchema = schemas.getFirst();
     assertEquals(base + "/html", htmlSchema.id());
     assertEquals(base + ".html", htmlSchema.contentUrl());
     assertEquals("text/html", htmlSchema.encodingFormat());
     assertEquals("de", htmlSchema.inLanguage());
 
-    // XML Schema
     CaseLawEncodingSchema xmlSchema = schemas.get(1);
     assertEquals(base + "/xml", xmlSchema.id());
     assertEquals(base + ".xml", xmlSchema.contentUrl());
     assertEquals("application/xml", xmlSchema.encodingFormat());
     assertEquals("de", xmlSchema.inLanguage());
 
-    // ZIP Schema
     CaseLawEncodingSchema zipSchema = schemas.get(2);
     assertEquals(base + "/zip", zipSchema.id());
     assertEquals(base + ".zip", zipSchema.contentUrl());
