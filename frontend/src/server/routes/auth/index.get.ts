c7cc4a30e809b3a90b659158ed2efbd64db6ef99
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-07-14 10:52:45 +0200

    Use typed event handler for auth/keycloak route

--- frontend/src/server/routes/auth/index.get.ts
+++ frontend/src/server/routes/auth/index.get.ts
@@ -1,10 +1,12 @@
 import { sendRedirect } from "h3";
 import { authRedirectCookieName } from "~/server/auth";
 
-export default defineEventHandler((event) => {
-  const query = getQuery(event);
-  if (query.redirectTo) {
-    setCookie(event, authRedirectCookieName, query.redirectTo.toString());
-  }
-  return sendRedirect(event, "/auth/keycloak");
-});
+export default defineEventHandler<{ query: { redirectTo?: string } }>(
+  (event) => {
+    const query = getQuery(event);
+    if (query.redirectTo) {
+      setCookie(event, authRedirectCookieName, query.redirectTo);
+    }
+    return sendRedirect(event, "/auth/keycloak");
+  },
+);
