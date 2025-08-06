b74bea9df05da5013d0a6c2778e34713a2f808b9
Haytham Mones <haythameyd@gmail.com>
2025-08-06 11:50:09 +0200

    Refactored menu items

--- frontend/src/components/ActionMenu/LargeActionsMenu.vue
+++ frontend/src/components/ActionMenu/LargeActionsMenu.vue
@@ -5,22 +5,4 @@ import type { ActionMenuItem } from "~/components/ActionMenu/ActionsMenu.vue";
 const { actions } = defineProps<{ actions: ActionMenuItem[] }>();
 </script>
 
-<template>
-  <div class="flex items-center *:-mx-4">
-    <Button
-      v-for="item in actions"
-      :key="item.key"
-      v-tooltip.bottom="item.label"
-      text
-      :disabled="item.disabled"
-      :aria-label="item.label"
-      :href="item.url"
-      :as="item.url ? 'a' : undefined"
-      @click.prevent="item.command && item.command()"
-    >
-      <template #icon
-        ><component :is="(item as ActionMenuItem).iconComponent"
-      /></template>
-    </Button>
-  </div>
-</template>
+<template></template>
