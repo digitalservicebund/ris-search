d2fbad627d3cd3475870dbd810b32fc2f7db3f28
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-08-20 08:06:08 +0200

    WIP: Literature to open search entity mapper

---
+++ backend/src/main/java/de/bund/digitalservice/ris/search/mapper/literature/LiteratureLdmlToOpenSearchMapper.java
@@ -0,0 +1,68 @@
+package de.bund.digitalservice.ris.search.mapper.literature;
+
+import de.bund.digitalservice.ris.search.models.opensearch.Literature;
+import de.bund.digitalservice.ris.search.utils.XmlDocument;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import javax.xml.xpath.XPathExpressionException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.w3c.dom.Node;
+
+public class LiteratureLdmlToOpenSearchMapper {
+  private static final Logger logger = LogManager.getLogger(LiteratureLdmlToOpenSearchMapper.class);
+
+  private LiteratureLdmlToOpenSearchMapper() {}
+
+  public static Optional<Literature> parseLiteratureLdml(String xmlFile) {
+    try {
+      var xmlDocument = new XmlDocument(xmlFile.getBytes(StandardCharsets.UTF_8));
+      return Optional.of(
+          Literature.builder()
+              .documentNumber(extractDocumentNumber(xmlDocument))
+              .yearsOfPublication(extractYearsOfPublication(xmlDocument))
+              .documentTypes(extractDocumentTypes(xmlDocument))
+              .build());
+    } catch (Exception e) {
+      logger.warn("Error creating literature opensearch entity.", e);
+      return Optional.empty();
+    }
+  }
+
+  private static String extractDocumentNumber(XmlDocument xmlDocument) {
+    String documentNumber =
+        xmlDocument.getElementByXpath(
+            "//*[local-name()='FRBRExpression']/*[local-name()='FRBRalias' and @name='documentNumber']/@value");
+
+    if (documentNumber == null) {
+      throw new IllegalArgumentException("Literature ldml has no documentNumber");
+    }
+
+    return documentNumber;
+  }
+
+  private static List<String> extractYearsOfPublication(XmlDocument xmlDocument) {
+    return extractNodeListTextContents(
+        xmlDocument,
+        "//*[local-name()='veroeffentlichungsJahre']/*[local-name()='veroeffentlichungsJahr']");
+  }
+
+  private static List<String> extractDocumentTypes(XmlDocument xmlDocument) {
+    return extractNodeListTextContents(
+        xmlDocument,
+        "//*[local-name()='classification' and @source='doktyp']/*[local-name()='keyword']/@value");
+  }
+
+  private static List<String> extractNodeListTextContents(XmlDocument xmlDocument, String xPath) {
+    List<Node> nodes;
+    try {
+      nodes = XmlDocument.asList(xmlDocument.getNodesByXpath(xPath));
+    } catch (XPathExpressionException e) {
+      return Collections.emptyList();
+    }
+
+    return nodes.stream().map(Node::getTextContent).toList();
+  }
+}
