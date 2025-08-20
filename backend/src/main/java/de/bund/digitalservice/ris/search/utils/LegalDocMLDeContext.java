6cc2cad4bbfe45f169b7b8d67c8d8eb93790272e
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-08-20 16:41:57 +0200

    Make XmlDocument to support different xml namespaces

--- backend/src/main/java/de/bund/digitalservice/ris/search/utils/LegalDocMLDeContext.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/utils/LegalDocMLDeContext.java
@@ -1,33 +1,8 @@
 package de.bund.digitalservice.ris.search.utils;
 
-import java.util.Iterator;
-import javax.xml.namespace.NamespaceContext;
+class LegalDocMLDeContext extends LegalDocMLNamespaceContext {
 
-class LegalDocMLDeContext implements NamespaceContext {
-  @Override
-  public String getNamespaceURI(String prefix) {
-    if ("akn".equals(prefix)) {
-      return "http://Inhaltsdaten.LegalDocML.de/1.8.2/";
-    }
-    if ("ris".equals(prefix)) {
-      return "http://MetadatenRIS.LegalDocML.de/1.8.2/";
-    }
-    return null;
-  }
-
-  @Override
-  public String getPrefix(String namespaceURI) {
-    if ("http://Inhaltsdaten.LegalDocML.de/1.8.2/".equals(namespaceURI)) {
-      return "akn";
-    }
-    if ("http://MetadatenRIS.LegalDocML.de/1.8.2/".equals(namespaceURI)) {
-      return "ris";
-    }
-    return null;
-  }
-
-  @Override
-  public Iterator<String> getPrefixes(String namespaceURI) {
-    return null;
+  protected LegalDocMLDeContext() {
+    super("http://Inhaltsdaten.LegalDocML.de/1.8.1/", "http://MetadatenRIS.LegalDocML.de/1.8.1/");
   }
 }
