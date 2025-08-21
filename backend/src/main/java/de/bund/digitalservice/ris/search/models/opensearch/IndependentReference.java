7a8fd7f0e28cc9fb7f7499487e00ef91f5deed0a
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-08-21 15:30:51 +0200

    Remove unused function and fix misspelling

--- backend/src/main/java/de/bund/digitalservice/ris/search/models/opensearch/IndependentReference.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/models/opensearch/IndependentReference.java
@@ -3,7 +3,7 @@ package de.bund.digitalservice.ris.search.models.opensearch;
 import lombok.Builder;
 import org.springframework.data.elasticsearch.annotations.Field;
 
-/** Represents a dependent reference (selbstständige Fundstelle) in a literature document. */
+/** Represents an independent reference (selbstständige Fundstelle) in a literature document. */
 @Builder
 public record IndependentReference(
     @Field(name = "title") String title, @Field(name = "citation") String citation) {}
