a0b8c65eebe273773b949bbd39317f9aa707cbc4
UrsKahmann <33418462+UrsKahmann@users.noreply.github.com>
2025-08-28 10:22:23 +0200

    Refactor ActionsMenu (#1125)
    Co-authored-by: Haytham Mones <haythameyd@gmail.com>

---
+++ frontend/src/utils/actionMenuUtils.ts
@@ -0,0 +1,68 @@
+import type {
+  ActionMenuItem,
+  ActionsMenuProps,
+} from "~/components/ActionMenu/ActionsMenu.vue";
+import PDFIcon from "~/components/icons/PDFIcon.vue";
+import UpdatingLinkIcon from "~/components/icons/UpdatingLinkIcon.vue";
+import XMLIcon from "~/components/icons/XMLIcon.vue";
+import MaterialSymbolsLink from "~icons/material-symbols/link";
+import MaterialSymbolsPrint from "~icons/material-symbols/print";
+
+export function createActionMenuItems(
+  records: ActionsMenuProps,
+  copyUrlCommand: (url: string) => Promise<void>,
+  navigationCommand: (xmlUrl: string) => Promise<void>,
+): ActionMenuItem[] {
+  const items: ActionMenuItem[] = [];
+  const link = records.link;
+  const xmlUrl = records.xmlUrl;
+
+  if (link) {
+    items.push({
+      key: "link",
+      label: link.label ?? "Link kopieren",
+      iconComponent: UpdatingLinkIcon,
+      command: async () => await copyUrlCommand(link.url),
+      url: link.url,
+    });
+  }
+
+  items.push(
+    ...[
+      {
+        key: "permalink",
+        label: records.permalink.label,
+        iconComponent: MaterialSymbolsLink,
+        command: async () => await copyUrlCommand(records.permalink.url),
+        url: records.permalink.url,
+        disabled: records.permalink.disabled,
+      },
+      {
+        key: "print",
+        label: "Drucken",
+        iconComponent: MaterialSymbolsPrint,
+        command: () => {
+          if (window) window.print();
+        },
+      },
+      {
+        key: "pdf",
+        label: "Als PDF speichern",
+        iconComponent: PDFIcon,
+        disabled: true,
+      },
+    ],
+  );
+
+  if (xmlUrl) {
+    items.push({
+      key: "xml",
+      label: "XML anzeigen",
+      iconComponent: XMLIcon,
+      command: async () => await navigationCommand(xmlUrl),
+      url: records.xmlUrl,
+      dataAttribute: "xml-view",
+    });
+  }
+  return items;
+}
---
+++ frontend/src/utils/actionMenuUtils.unit.spec.ts
@@ -0,0 +1,183 @@
+import { vi } from "vitest";
+import type { ActionMenuItem } from "~/components/ActionMenu/ActionsMenu.vue";
+import PDFIcon from "~/components/icons/PDFIcon.vue";
+import UpdatingLinkIcon from "~/components/icons/UpdatingLinkIcon.vue";
+import XMLIcon from "~/components/icons/XMLIcon.vue";
+import { createActionMenuItems } from "~/utils/actionMenuUtils";
+import MaterialSymbolsLink from "~icons/material-symbols/link";
+import MaterialSymbolsPrint from "~icons/material-symbols/print";
+
+const { mockNavigationCommand, mockCopyUrlCommand } = vi.hoisted(() => ({
+  mockNavigationCommand: vi.fn(),
+  mockCopyUrlCommand: vi.fn(),
+}));
+
+const minimalExpectedActions = [
+  {
+    key: "permalink",
+    label: "Link kopieren",
+    iconComponent: MaterialSymbolsLink,
+    url: "https://permalink.com/",
+  },
+  {
+    key: "print",
+    label: "Drucken",
+    iconComponent: MaterialSymbolsPrint,
+  },
+  {
+    key: "pdf",
+    label: "Als PDF speichern",
+    iconComponent: PDFIcon,
+    disabled: true,
+  },
+];
+
+function findCommandForActionWithKey(actions: ActionMenuItem[], key: string) {
+  return actions.find((action) => action.key === key)?.command;
+}
+
+describe("actionMenuUtils.ts", () => {
+  it("returns permalink, print and pdf actions if only permalink is given", () => {
+    const result = createActionMenuItems(
+      {
+        permalink: {
+          url: "https://permalink.com/",
+          label: "Link kopieren",
+        },
+      },
+      mockCopyUrlCommand,
+      mockNavigationCommand,
+    );
+
+    expect(result).toMatchObject(minimalExpectedActions);
+  });
+
+  it("prepends link action if given", () => {
+    const result = createActionMenuItems(
+      {
+        link: {
+          url: "https://link.com/",
+          label: "Copy link",
+        },
+        permalink: {
+          url: "https://permalink.com/",
+          label: "Link kopieren",
+          disabled: true,
+        },
+      },
+      mockCopyUrlCommand,
+      mockNavigationCommand,
+    );
+
+    const expectedActions = [
+      {
+        key: "link",
+        label: "Copy link",
+        iconComponent: UpdatingLinkIcon,
+        url: "https://link.com/",
+      },
+      ...minimalExpectedActions,
+    ];
+
+    expect(result).toMatchObject(expectedActions);
+  });
+
+  it("appends xml action if given", () => {
+    const result = createActionMenuItems(
+      {
+        permalink: {
+          url: "https://permalink.com/",
+          label: "Link kopieren",
+          disabled: true,
+        },
+        xmlUrl: "https://xml.xml",
+      },
+      mockCopyUrlCommand,
+      mockNavigationCommand,
+    );
+
+    const expectedActions = [
+      ...minimalExpectedActions,
+      {
+        key: "xml",
+        label: "XML anzeigen",
+        iconComponent: XMLIcon,
+        url: "https://xml.xml",
+        dataAttribute: "xml-view",
+      },
+    ];
+
+    expect(result).toMatchObject(expectedActions);
+  });
+
+  it("sets copyLink callback for link actions", async () => {
+    const result = createActionMenuItems(
+      {
+        link: {
+          url: "https://link.com/",
+          label: "Copy link",
+        },
+        permalink: {
+          url: "https://permalink.com/",
+          label: "Copy permalink",
+        },
+      },
+      mockCopyUrlCommand,
+      mockNavigationCommand,
+    );
+
+    const copyLinkCommand = findCommandForActionWithKey(result, "link");
+    copyLinkCommand?.();
+    expect(mockCopyUrlCommand).toHaveBeenCalledWith("https://link.com/");
+
+    const copyPermalinkCommand = findCommandForActionWithKey(
+      result,
+      "permalink",
+    );
+    copyPermalinkCommand?.();
+    expect(mockCopyUrlCommand).toHaveBeenCalledWith("https://permalink.com/");
+  });
+
+  it("print action command opens print dialog", () => {
+    const printSpy = vi.spyOn(window, "print").mockImplementation(() => {});
+    const result = createActionMenuItems(
+      {
+        permalink: {
+          url: "https://permalink.com/",
+          label: "Link kopieren",
+          disabled: true,
+        },
+      },
+      mockCopyUrlCommand,
+      mockNavigationCommand,
+    );
+
+    const printCommand = findCommandForActionWithKey(result, "print");
+    printCommand?.();
+
+    expect(printSpy).toHaveBeenCalled();
+
+    printSpy.mockRestore();
+  });
+
+  it("can navigate to xml", () => {
+    const result = createActionMenuItems(
+      {
+        permalink: {
+          url: "",
+          label: "Foo",
+        },
+        xmlUrl: "https://xml.xml",
+      },
+      mockCopyUrlCommand,
+      mockNavigationCommand,
+    );
+
+    const xmlCommand = findCommandForActionWithKey(result, "xml");
+    xmlCommand?.();
+
+    expect(mockNavigationCommand).toHaveBeenCalledExactlyOnceWith(
+      "https://xml.xml",
+    );
+  });
+});
