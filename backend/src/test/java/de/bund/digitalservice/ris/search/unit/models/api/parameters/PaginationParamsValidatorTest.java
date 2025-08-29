60e7b401f520f90a662f5f9ea9b573399568c9f8
ekl176 <eric.klemm@digitalservice.bund.de>
2025-08-29 14:39:43 +0200

    validate pageIndex on dto (#1128)

--- backend/src/test/java/de/bund/digitalservice/ris/search/unit/models/api/parameters/PaginationParamsValidatorTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/unit/models/api/parameters/PaginationParamsValidatorTest.java
@@ -6,9 +6,13 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import java.util.Set;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PaginationParamsValidatorTest {
 
@@ -16,26 +20,50 @@ class PaginationParamsValidatorTest {
 
   @BeforeAll
   static void setUpClass() {
-    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-    validator = factory.getValidator();
+    try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+      validator = factory.getValidator();
+    }
   }
 
-  @Test
-  void itDetectsViolationWhenResultWindowIsExceeded() {
-    PaginationParams params = new PaginationParams();
-    params.setSize(100);
-    params.setPageIndex(100);
+  private static Stream<Arguments> provideInvalidPaginationTestArguments() {
+    PaginationParams invalidResultWindow = new PaginationParams();
+    invalidResultWindow.setPageIndex(100);
+    invalidResultWindow.setSize(100);
+
+    PaginationParams invalidPageIndex = new PaginationParams();
+    invalidPageIndex.setSize(100);
+    invalidPageIndex.setPageIndex(-1);
+
+    PaginationParams sizeTooBig = new PaginationParams();
+    sizeTooBig.setSize(101);
+    sizeTooBig.setPageIndex(1);
+
+    PaginationParams sizeTooSmall = new PaginationParams();
+    sizeTooSmall.setSize(0);
+    sizeTooSmall.setPageIndex(1);
+
+    return Stream.of(
+        Arguments.of(
+            invalidResultWindow, "PageIndex out of Bounds. Result window must not exceed 10000"),
+        Arguments.of(invalidPageIndex, "pageIndex must be at least 0"),
+        Arguments.of(sizeTooBig, "size must not exceed 100"),
+        Arguments.of(sizeTooSmall, "size must be at least 1"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideInvalidPaginationTestArguments")
+  void itDetectsViolationsAndSetsTheCorrectMessage(
+      PaginationParams params, String expectedMessage) {
     Set<ConstraintViolation<PaginationParams>> constraints = validator.validate(params);
 
     Assertions.assertEquals(1, constraints.size());
     ConstraintViolation<PaginationParams> constraint = constraints.iterator().next();
 
-    Assertions.assertEquals(
-        "PageIndex out of Bounds. Result window must not exceed 10000", constraint.getMessage());
+    Assertions.assertEquals(expectedMessage, constraint.getMessage());
   }
 
   @Test
-  void itLetsCorrectResultWindowPass() {
+  void itLetsCorrectParametersPass() {
     PaginationParams params = new PaginationParams();
     params.setSize(100);
     params.setPageIndex(99);
