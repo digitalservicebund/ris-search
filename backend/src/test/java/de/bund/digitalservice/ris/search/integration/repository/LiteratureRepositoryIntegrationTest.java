192c332bfeb33738faeb54a82fdae00d314c4489
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-09-10 10:08:07 +0200

    Fix literature repository integration test

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/repository/LiteratureRepositoryIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/repository/LiteratureRepositoryIntegrationTest.java
@@ -2,16 +2,19 @@ package de.bund.digitalservice.ris.search.integration.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import de.bund.digitalservice.ris.search.integration.config.ContainersIntegrationBase;
 import de.bund.digitalservice.ris.search.models.opensearch.Literature;
 import de.bund.digitalservice.ris.search.repository.opensearch.LiteratureRepository;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LiteratureRepositoryIntegrationTest {
+@Tag("integration")
+class LiteratureRepositoryIntegrationTest extends ContainersIntegrationBase {
 
   @Autowired private LiteratureRepository literatureRepository;
 
