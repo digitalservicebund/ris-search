9706ff44259e835da76e317b3dbe14426bcbdb98
Haytham Mones <haythameyd@gmail.com>
2025-09-11 13:42:31 +0200

    Refactored the way we retrieve files

--- frontend/src/server/routes/robots.txt.get.ts
+++ frontend/src/server/routes/robots.txt.get.ts
@@ -1,16 +1,16 @@
-import { promises } from "node:fs";
-import { join } from "node:path";
+import { getRequestURL, getHeader, setHeader } from "h3";
 import { isPublicProfile } from "~/utils/config";
 
 export default defineEventHandler(async (event) => {
   const userAgent = (getHeader(event, "User-Agent") ?? "").toUpperCase();
-  let assetPath = isPublicProfile()
-    ? "robots.public.txt"
-    : "robots.staging.txt";
-  if (userAgent === "DG_JUSTICE_CRAWLER") assetPath = "robots.dg.txt";
+  let file = isPublicProfile() ? "robots.public.txt" : "robots.staging.txt";
+  if (userAgent === "DG_JUSTICE_CRAWLER") file = "robots.dg.txt";
 
   setHeader(event, "Content-Type", "text/plain; charset=utf-8");
+  setHeader(event, "Vary", "User-Agent");
 
-  const filePath = join(process.cwd(), "src/public", assetPath);
-  return await promises.readFile(filePath, "utf-8");
+  const { origin } = getRequestURL(event);
+  const url = `${origin}/${file}`;
+
+  return await $fetch<string>(url, { method: "GET" });
 });
