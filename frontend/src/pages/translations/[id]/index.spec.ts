7109704e87830f476e68f7c04c1017738f2bd7da
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2025-09-11 15:05:23 +0200

    Revert "Add another test"
    This reverts commit 204c6e78df5c03a00b5761460f34ed783fdd7808.

--- frontend/src/pages/translations/[id]/index.spec.ts
+++ frontend/src/pages/translations/[id]/index.spec.ts
@@ -4,69 +4,27 @@ import { ref } from "vue";
 import TranslationDetailPage from "~/pages/translations/[id]/index.vue";
 import type { TranslationData } from "~/pages/translations/useTranslationData";
 
-describe("translations detail page", async () => {
-  it("displays translation detail page correctly and replaces prefixes", async () => {
-    const mockTranslationData = vi.hoisted<TranslationData>(() => ({
-      content: {
-        "@id": "AbC",
-        name: "Act A",
-        inLanguage: "en",
-        translator: "Translation provided by someone.",
-        translationOfWork: "Gesetz A",
-        about: "Version information: The translation includes text",
-        "ris:filename": "englisch_abc.html",
-      },
-      html: '<html><body><p style="text-align: center; font-weight: bold">Chapter 1</p></body></html>',
-    }));
+const mockTranslationData: TranslationData = {
+  content: {
+    "@id": "AbC",
+    name: "Act A",
+    inLanguage: "en",
+    translator: "Translation provided by someone.",
+    translationOfWork: "Gesetz A",
+    about: "Version information: The translation includes text",
+    "ris:filename": "englisch_abc.html",
+  },
+  html: '<html><body><p style="text-align: center; font-weight: bold">Chapter 1</p></body></html>',
+};
 
-    vi.mock("~/pages/translations/useTranslationData", () => ({
-      fetchTranslationAndHTML: vi.fn(() => ({
-        data: ref(mockTranslationData),
-      })),
-    }));
-
-    const wrapper = await mountSuspended(TranslationDetailPage);
-
-    const pageHeader = wrapper.find("h1");
-    expect(pageHeader.exists()).toBe(true);
-    expect(pageHeader.text()).toContain("Act A");
-
-    const allParagraphs = wrapper.findAll("p");
-    const actAParagraph = allParagraphs.find((p) => p.text() === "Chapter 1");
-    expect(actAParagraph?.exists()).toBe(true);
-
-    const tabButton = wrapper.get("button[aria-label*='Details']");
-    await tabButton.trigger("click");
-
-    const metaData = wrapper.findAll("dd");
-    expect(metaData.length).toBe(2);
-
-    expect(metaData[0].text()).toBe("someone.");
-    expect(metaData[1].text()).toBe("The translation includes text");
-  });
-});
+vi.mock("~/pages/translations/useTranslationData", () => ({
+  fetchTranslationAndHTML: vi.fn(() => ({
+    data: ref(mockTranslationData),
+  })),
+}));
 
 describe("translations detail page", async () => {
-  it("displays translation detail page correctly and does not replaces prefixes", async () => {
-    const mockTranslationData1 = vi.hoisted<TranslationData>(() => ({
-      content: {
-        "@id": "AbC",
-        name: "Act A",
-        inLanguage: "en",
-        translator: "someone.",
-        translationOfWork: "Gesetz A",
-        about: "The translation includes text",
-        "ris:filename": "englisch_abc.html",
-      },
-      html: '<html><body><p style="text-align: center; font-weight: bold">Chapter 1</p></body></html>',
-    }));
-
-    vi.mock("~/pages/translations/useTranslationData", () => ({
-      fetchTranslationAndHTML: vi.fn(() => ({
-        data: ref(mockTranslationData1),
-      })),
-    }));
-
+  it("displays translation detail page correctly", async () => {
     const wrapper = await mountSuspended(TranslationDetailPage);
 
     const pageHeader = wrapper.find("h1");
