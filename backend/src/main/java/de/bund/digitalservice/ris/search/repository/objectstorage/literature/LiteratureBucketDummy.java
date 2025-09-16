9b71808cedb212a36d777d060666957c90c0f496
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2025-09-16 07:53:06 +0200

    Add test for literature bucket

--- backend/src/main/java/de/bund/digitalservice/ris/search/repository/objectstorage/literature/LiteratureBucketDummy.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/repository/objectstorage/literature/LiteratureBucketDummy.java
@@ -25,43 +25,53 @@ public class LiteratureBucketDummy extends LiteratureBucket {
     super(null, null);
   }
 
+  @Override
   public List<String> getAllKeys() {
     return Collections.emptyList();
   }
 
+  @Override
   public List<String> getAllKeysByPrefix(String path) {
     return Collections.emptyList();
   }
 
+  @Override
   public Optional<String> getFileAsString(String filename) throws ObjectStoreServiceException {
     Optional<byte[]> s3Response = get(filename);
     return s3Response.map(bytes -> new String(bytes, StandardCharsets.UTF_8));
   }
 
+  @Override
   public Optional<byte[]> get(String objectKey) throws ObjectStoreServiceException {
     return Optional.empty();
   }
 
+  @Override
   public FilterInputStream getStream(String objectKey) throws NoSuchKeyException {
     throw new NoSuchKeyException("Key not found", new ObjectStoreServiceException("Key not found"));
   }
 
+  @Override
   public List<ObjectKeyInfo> getAllKeyInfosByPrefix(String path) {
     return Collections.emptyList();
   }
 
+  @Override
   public void delete(String fileName) {
     /**/
   }
 
+  @Override
   public void save(String fileName, String fileContent) {
     /**/
   }
 
+  @Override
   public void close() {
     /**/
   }
 
+  @Override
   public long putStream(String objectKey, InputStream inputStream) throws IOException {
     return 0;
   }
