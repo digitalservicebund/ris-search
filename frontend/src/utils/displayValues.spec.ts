cd64555ff861a77e1257d98e2fd38deb64522cd7
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-09-17 16:12:56 +0200

    feat: add util for getting labels for document kinds
    RISDEV-8385

---
+++ frontend/src/utils/displayValues.spec.ts
@@ -0,0 +1,27 @@
+import { formatDocumentKind } from "./displayValues";
+import { DocumentKind } from "~/types";
+
+describe("displayValues", () => {
+  describe("formatDocumentKind", () => {
+    it("formats caselaw", () => {
+      expect(formatDocumentKind(DocumentKind.CaseLaw)).toBe(
+        "Gerichtsentscheidungen",
+      );
+    });
+
+    it("formats norms", () => {
+      expect(formatDocumentKind(DocumentKind.Norm)).toBe(
+        "Gesetze und Verordnungen",
+      );
+    });
+
+    it("formats all", () => {
+      expect(formatDocumentKind(DocumentKind.All)).toBe("Datensätze");
+    });
+
+    it("returns all as the fallback", () => {
+      // @ts-expect-error -- Intentionally breaking for testing purposes
+      expect(formatDocumentKind(null)).toBe("Datensätze");
+    });
+  });
+});
---
+++ frontend/src/utils/displayValues.ts
@@ -0,0 +1,17 @@
+import { DocumentKind } from "~/types";
+
+const documentKindDisplayValues: Record<DocumentKind, string> = {
+  [DocumentKind.CaseLaw]: "Gerichtsentscheidungen",
+  [DocumentKind.Norm]: "Gesetze und Verordnungen",
+  [DocumentKind.All]: "Datensätze",
+};
+
+/**
+ * Returns a human readable label for the specified document kind.
+ *
+ * @param documentKind Document kind to be displayed
+ * @returns Display value
+ */
+export function formatDocumentKind(documentKind: DocumentKind): string {
+  return documentKindDisplayValues[documentKind] ?? documentKindDisplayValues.A;
+}
