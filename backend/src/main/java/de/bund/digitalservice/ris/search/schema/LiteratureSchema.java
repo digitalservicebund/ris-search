6f824542ccfb69cb6be5d851ab77a044c9046530
UrsKahmann <33418462+UrsKahmann@users.noreply.github.com>
2025-09-18 14:51:44 +0200

    RISDEV-8941: Create schema.org schema for literature (#1169)
    - adds a schema.org schema for literature
    - refactor how the encoding schema entities are created for all document
    types

---
+++ backend/src/main/java/de/bund/digitalservice/ris/search/schema/LiteratureSchema.java
@@ -0,0 +1,33 @@
+package de.bund.digitalservice.ris.search.schema;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldId;
+import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldResource;
+import ioinformarics.oss.jackson.module.jsonld.annotation.JsonldType;
+import java.util.List;
+import lombok.Builder;
+import org.jetbrains.annotations.Nullable;
+
+@Builder
+@JsonldResource
+@JsonldType("Literature")
+public record LiteratureSchema(
+    @Schema(example = "KALU000000000") @JsonldId String id,
+    @Schema(example = "de") String inLanguage,
+    @Schema(description = "Dokumentnummer", example = "KALU000000000") String documentNumber,
+    @Schema(description = "Veröffentlichungsjahre", example = "[2014, 2024-09]")
+        List<String> yearsOfPublication,
+    @Schema(description = "Dokumenttypen", example = "[Auf]") List<String> documentTypes,
+    @Schema(description = "Unselbstständige Fundstellen", example = "[BUV, 1982, 123-123]")
+        List<String> dependentReferences,
+    @Schema(
+            description = "Selbstständige Fundstellen",
+            example = "[50 Jahre Betriebs-Berater, 1987, 123-456]")
+        List<String> independentReferences,
+    @Schema(description = "Haupttitel") String headline,
+    @Schema(description = "Dokumentarischer Titel") String alternativeTitle,
+    @Schema(description = "Authoren", example = "[Musterfrau, Sabine]") List<String> authors,
+    @Schema(description = "Mitarbeiter", example = "[Mustermann, Max]") List<String> collaborators,
+    @Schema(description = "Kurzrefarat") String shortReport,
+    @Schema(description = "Gliederung") String outline,
+    @Nullable List<LiteratureEncodingSchema> encoding) {}
