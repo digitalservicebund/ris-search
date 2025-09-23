05742359e9ea90b8cb21c58853332572fa30b10e
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2025-09-23 17:46:05 +0200

    Add test

---
+++ frontend/src/composables/useTranslationData.unit.spec.ts
@@ -0,0 +1,98 @@
+import { mockNuxtImport } from "@nuxt/test-utils/runtime";
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import {
+  fetchTranslationList,
+  fetchTranslationAndHTML,
+} from "~/composables/useTranslationData";
+
+const mockTranslationListData: TranslationContent[] = [
+  {
+    "@id": "Cde",
+    name: "Act B",
+    inLanguage: "en",
+    translator: "…",
+    translationOfWork: "Gesetz B",
+    about: "…",
+    "ris:filename": "englisch_cde.html",
+  },
+  {
+    "@id": "AbC",
+    name: "Act A",
+    inLanguage: "en",
+    translator: "…",
+    translationOfWork: "Gesetz A",
+    about: "…",
+    "ris:filename": "englisch_abc.html",
+  },
+];
+
+vi.mock("~/composables/useBackendURL", () => {
+  return {
+    useBackendURL: () => "http://mock-backend",
+  };
+});
+
+// mock useRequestFetch to return our mock data
+const requestFetchMock = vi.fn();
+
+mockNuxtImport("useRequestFetch", () => {
+  return () => requestFetchMock;
+});
+
+beforeEach(() => {
+  requestFetchMock.mockReset();
+});
+
+describe("fetchTranslationList", () => {
+  it("returns a list when there is no error", async () => {
+    requestFetchMock.mockResolvedValueOnce(mockTranslationListData);
+    const { data, error } = await fetchTranslationList();
+
+    expect(requestFetchMock).toHaveBeenCalledWith(
+      "http://mock-backend/v1/translatedLegislation",
+    );
+    expect(error.value).toBeNull();
+    expect(data.value).toHaveLength(2);
+  });
+});
+
+describe("fetchTranslationAndHTML", () => {
+  it("fetches translation data and HTML content", async () => {
+    const mockTranslationResponse = [
+      {
+        "@id": "AbC",
+        name: "Act A",
+        inLanguage: "en",
+        translator: "…",
+        translationOfWork: "Gesetz A",
+        about: "…",
+        "ris:filename": "englisch_abc.html",
+      },
+    ];
+
+    const mockHtmlResponse = "<h1>Mock HTML Content</h1>";
+
+    requestFetchMock.mockResolvedValueOnce(mockTranslationResponse);
+    requestFetchMock.mockResolvedValueOnce(mockHtmlResponse);
+
+    const { data, error } = await fetchTranslationAndHTML("AbC");
+
+    expect(requestFetchMock).toHaveBeenCalledWith(
+      "http://mock-backend/v1/translatedLegislation?id=AbC",
+    );
+
+    expect(requestFetchMock).toHaveBeenCalledWith(
+      "http://mock-backend/v1/translatedLegislation/englisch_abc.html",
+      {
+        headers: {
+          Accept: "text/html",
+        },
+      },
+    );
+
+    expect(error.value).toBeNull();
+
+    expect(data.value.content).toEqual(mockTranslationResponse[0]);
+    expect(data.value.html).toEqual(mockHtmlResponse);
+  });
+});
