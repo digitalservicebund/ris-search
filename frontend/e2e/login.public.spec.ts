b6f88af637745ba57b0141bf64ab9773d9aef0e3
Haytham Mones <haythameyd@gmail.com>
2025-09-24 14:38:17 +0200

    skipped login on mobile

--- frontend/e2e/login.public.spec.ts
+++ frontend/e2e/login.public.spec.ts
@@ -1,8 +1,9 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 const displayName = process.env.E2E_KEYCLOAK_USER_DISPLAY_NAME ?? "Jane Doe";
 
-test("is authenticated", async ({ page }) => {
+test("is authenticated", async ({ page }, workerInfo) => {
+  test.skip(workerInfo.project.name === "mobile");
   // authentication should happen in the setup flow in auth.setup.ts
   await page.goto("/");
 
