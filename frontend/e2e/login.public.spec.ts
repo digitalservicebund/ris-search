a9da8b0bd800b6574adc55a050ed18a1792e7f9e
Haytham Mones <haythameyd@gmail.com>
2025-09-24 14:40:52 +0200

    Added option for mobile menu to check on the logout button

--- frontend/e2e/login.public.spec.ts
+++ frontend/e2e/login.public.spec.ts
@@ -3,10 +3,9 @@ import { expect, test } from "@playwright/test";
 const displayName = process.env.E2E_KEYCLOAK_USER_DISPLAY_NAME ?? "Jane Doe";
 
 test("is authenticated", async ({ page }, workerInfo) => {
-  test.skip(workerInfo.project.name === "mobile");
-  // authentication should happen in the setup flow in auth.setup.ts
   await page.goto("/");
-
+  if (workerInfo.project.name === "mobile")
+    await page.getByRole("button", { name: "Menu" }).click();
   const expectedLogoutButtonName = `${displayName} Abmelden`;
   await expect(page.getByText(expectedLogoutButtonName)).toBeVisible();
 });
