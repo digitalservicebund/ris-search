a3da601658bd0fbfe73dd44a263d5cd603e1b764
Haytham Mones <haythameyd@gmail.com>
2025-09-26 10:34:25 +0200

    Testnig a fix for the authenticated view on mobile

--- frontend/e2e/login.public.spec.ts
+++ frontend/e2e/login.public.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 const displayName = process.env.E2E_KEYCLOAK_USER_DISPLAY_NAME ?? "Jane Doe";
 
@@ -6,6 +6,6 @@ test("is authenticated", async ({ page }, workerInfo) => {
   await page.goto("/");
   if (workerInfo.project.name === "mobile")
     await page.getByRole("button", { name: "Menu" }).click();
-  const expectedLogoutButtonName = `${displayName} Abmelden`;
-  await expect(page.getByText(expectedLogoutButtonName).first()).toBeVisible();
+  await page.getByRole("button", { name: `Abmelden` }).isVisible();
+  await page.getByText(displayName).isVisible();
 });
