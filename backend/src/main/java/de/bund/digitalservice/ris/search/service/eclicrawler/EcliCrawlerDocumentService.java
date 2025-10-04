2eb28eec6bfd9edf9236c2bdcff1020eeef6b6b2
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-10-04 10:42:10 +0200

    avoid keeping all documents in memory before writing
    adjust test
    
    return filenames, not whole documents in ecli repo
    
    use blockingArrayQueue as buffer

--- backend/src/main/java/de/bund/digitalservice/ris/search/service/eclicrawler/EcliCrawlerDocumentService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/service/eclicrawler/EcliCrawlerDocumentService.java
@@ -5,13 +5,15 @@ import de.bund.digitalservice.ris.search.exception.ObjectStoreServiceException;
 import de.bund.digitalservice.ris.search.importer.changelog.Changelog;
 import de.bund.digitalservice.ris.search.mapper.EcliCrawlerDocumentMapper;
 import de.bund.digitalservice.ris.search.models.eclicrawler.ecli.Courts;
+import de.bund.digitalservice.ris.search.models.eclicrawler.sitemap.Sitemap;
 import de.bund.digitalservice.ris.search.models.opensearch.CaseLawDocumentationUnit;
 import de.bund.digitalservice.ris.search.models.opensearch.EcliCrawlerDocument;
 import de.bund.digitalservice.ris.search.repository.objectstorage.CaseLawBucket;
 import de.bund.digitalservice.ris.search.repository.opensearch.EcliCrawlerDocumentRepository;
-import de.bund.digitalservice.ris.search.service.BulkChangelogParser;
 import de.bund.digitalservice.ris.search.service.CaseLawService;
 import de.bund.digitalservice.ris.search.service.IndexSyncJob;
+import jakarta.xml.bind.JAXBException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -30,8 +32,10 @@ public class EcliCrawlerDocumentService {
   CaseLawBucket caselawBucket;
   EcliCrawlerDocumentRepository repository;
   CaseLawService caselawService;
+  EcliSitemapService sitemapService;
   private final String documentUrl;
   private static final int DOCUMENT_STORE_BULK_SIZE = 10000;
+  public static final int MAX_SITEMAP_URLS = 50000;
   private static final Logger logger = LogManager.getLogger(EcliCrawlerDocumentService.class);
 
   // ecli regex provided by the ecli xsd
@@ -42,68 +46,17 @@ public class EcliCrawlerDocumentService {
       CaseLawBucket caseLawBucket,
       EcliCrawlerDocumentRepository repository,
       CaseLawService caselawService,
+      EcliSitemapService sitemapService,
       @Value("${server.front-end-url}") String frontEndUrl) {
     this.caselawBucket = caseLawBucket;
     this.repository = repository;
     this.caselawService = caselawService;
+    this.sitemapService = sitemapService;
     this.documentUrl = frontEndUrl + "case-law/";
   }
 
   public void saveAll(List<EcliCrawlerDocument> docs) {
     ListUtils.partition(docs, DOCUMENT_STORE_BULK_SIZE).forEach(i -> repository.saveAll(i));
-  }
-
-  public List<EcliCrawlerDocument> getFromChangelogs(List<Changelog> changelogs) {
-    if (BulkChangelogParser.containsChangeAll(changelogs)) {
-      return getFullDiff();
-    } else {
-      return getAllEcliCrawlerDocumentsFromChangelog(
-          BulkChangelogParser.mergeChangelogs(changelogs));
-    }
-  }
-
-  public List<EcliCrawlerDocument> getFullDiff() {
-    List<String> allFiles =
-        caselawBucket.getAllKeys().stream()
-            .filter(s -> s.endsWith(".xml") && !s.contains(IndexSyncJob.CHANGELOGS_PREFIX))
-            .toList();
-
-    List<EcliCrawlerDocument> allEcliDocuments = new ArrayList<>(getEcliCrawlerDocuments(allFiles));
-    List<String> ecliFilenames =
-        allEcliDocuments.stream().map(EcliCrawlerDocument::filename).toList();
-
-    try (Stream<EcliCrawlerDocument> allPublished = repository.findAllByIsPublishedIsTrue()) {
-      var obsoleteDocuments =
-          allPublished
-              .filter(doc -> !ecliFilenames.contains(doc.filename()))
-              .map(this::setDeleted)
-              .toList();
-      allEcliDocuments.addAll(obsoleteDocuments);
-    }
-
-    return allEcliDocuments;
-  }
-
-  private List<EcliCrawlerDocument> getEcliCrawlerDocuments(List<String> filenames) {
-    List<EcliCrawlerDocument> ecliDocuments = new ArrayList<>();
-
-    for (String filename : filenames) {
-      try {
-        Optional<CaseLawDocumentationUnit> unitOption = caselawService.getFromBucket(filename);
-        unitOption.ifPresentOrElse(
-            unit -> {
-              if (isValidEcliDocument(unit)) {
-                ecliDocuments.add(
-                    EcliCrawlerDocumentMapper.fromCaseLawDocumentationUnit(
-                        documentUrl, filename, unit));
-              }
-            },
-            () -> logger.info("Unable to parse caseLawDocumentationunit {} - continue", filename));
-      } catch (ObjectStoreServiceException ex) {
-        throw new FatalEcliSitemapJobException(ex.getMessage());
-      }
-    }
-    return ecliDocuments;
   }
 
   private boolean isValidEcliDocument(CaseLawDocumentationUnit unit) {
@@ -115,19 +68,6 @@ public class EcliCrawlerDocumentService {
         && Courts.supportedCourtNames.containsKey(unit.courtType());
   }
 
-  private List<EcliCrawlerDocument> getAllEcliCrawlerDocumentsFromChangelog(
-      Changelog mergedChangelog) {
-
-    List<EcliCrawlerDocument> changes = new ArrayList<>();
-    changes.addAll(getEcliCrawlerDocuments(mergedChangelog.getChanged().stream().toList()));
-
-    changes.addAll(
-        repository.findAllByFilenameIn(mergedChangelog.getDeleted()).stream()
-            .map(this::setDeleted)
-            .toList());
-    return changes;
-  }
-
   private EcliCrawlerDocument setDeleted(EcliCrawlerDocument doc) {
     return new EcliCrawlerDocument(
         doc.documentNumber(),
@@ -137,5 +77,85 @@ public class EcliCrawlerDocumentService {
         doc.decisionDate(),
         doc.url(),
         false);
+  }
+
+  public void writeFromChangelog(String apiUrl, LocalDate day, Changelog changelog) {
+
+    ChangedEcliCrawlerDocumentsIterator iterator =
+        new ChangedEcliCrawlerDocumentsIterator(
+            this::getFromBucket,
+            this::getPublishedDocument,
+            new ArrayList<>(changelog.getChanged()),
+            new ArrayList<>(changelog.getDeleted()),
+            MAX_SITEMAP_URLS);
+
+    writeFilesFromIterator(apiUrl, day, iterator);
+  }
+
+  public void writeFullDiff(String apiUrl, LocalDate day) {
+    List<String> allFiles =
+        caselawBucket.getAllKeys().stream()
+            .filter(s -> s.endsWith(".xml") && !s.contains(IndexSyncJob.CHANGELOGS_PREFIX))
+            .toList();
+
+    List<String> toBeDeleted;
+    try (Stream<String> allPublished = repository.findFilenameByIsPublishedIsTrue()) {
+      toBeDeleted =
+          new ArrayList<>(allPublished.filter(filename -> !allFiles.contains(filename)).toList());
+    }
+
+    ChangedEcliCrawlerDocumentsIterator iterator =
+        new ChangedEcliCrawlerDocumentsIterator(
+            this::getFromBucket,
+            this::getPublishedDocument,
+            allFiles,
+            toBeDeleted,
+            MAX_SITEMAP_URLS);
+
+    writeFilesFromIterator(apiUrl, day, iterator);
+  }
+
+  private void writeFilesFromIterator(
+      String apiUrl, LocalDate day, ChangedEcliCrawlerDocumentsIterator iterator) {
+    try {
+      List<Sitemap> writtenSitemaps = new ArrayList<>();
+      while (iterator.hasNext()) {
+        var docs = iterator.next();
+        logger.info("write {} urls to sitemap", docs.size());
+        writtenSitemaps.add(
+            sitemapService.writeUrlsToSitemap(
+                day,
+                docs.stream().map(EcliCrawlerDocumentMapper::toSitemapUrl).toList(),
+                writtenSitemaps.size() + 1));
+        saveAll(docs);
+      }
+      logger.info("write {} sitemapindices", writtenSitemaps.size());
+      var sitemapIndices = sitemapService.writeSitemapsIndices(apiUrl, day, writtenSitemaps);
+      sitemapService.updateRobotsTxt(apiUrl, sitemapIndices);
+    } catch (JAXBException | ObjectStoreServiceException e) {
+      throw new FatalEcliSitemapJobException(e.getMessage());
+    }
+  }
+
+  private Optional<EcliCrawlerDocument> getFromBucket(String filename) {
+    try {
+      return caselawService
+          .getFromBucket(filename)
+          .flatMap(
+              unit -> {
+                if (isValidEcliDocument(unit)) {
+                  return Optional.of(
+                      EcliCrawlerDocumentMapper.fromCaseLawDocumentationUnit(
+                          documentUrl, filename, unit));
+                }
+                return Optional.empty();
+              });
+    } catch (ObjectStoreServiceException e) {
+      throw new FatalEcliSitemapJobException("no connection to bucket");
+    }
+  }
+
+  private Optional<EcliCrawlerDocument> getPublishedDocument(String filename) {
+    return repository.findByFilenameIn(filename).map(this::setDeleted);
   }
 }
