a2c4e46cb520658c00e46906d0819ef9edab6832
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-10-09 11:52:31 +0200

    allow empty ssr backend url

--- frontend/start.sh
+++ frontend/start.sh
@@ -5,7 +5,7 @@ set -euf
 export NUXT_OAUTH_KEYCLOAK_CLIENT_ID="${NUXT_OAUTH_KEYCLOAK_CLIENT_ID:=$(cat /etc/secrets/keycloakclientid/clientid)}"
 export NUXT_OAUTH_KEYCLOAK_CLIENT_SECRET="${NUXT_OAUTH_KEYCLOAK_CLIENT_SECRET:=$(cat /etc/secrets/keycloakclientsecret/clientsecret)}"
 export NUXT_RIS_BACKEND_URL="${NUXT_RIS_BACKEND_URL:=$(cat /etc/secrets/ris-backend-url/url)}"
-export NUXT_SSR_BACKEND_URL="${NUXT_SSR_BACKEND_URL:=$(cat /etc/secrets/ssr-backend-url/url)}"
+export NUXT_SSR_BACKEND_URL="${NUXT_SSR_BACKEND_URL=$(cat /etc/secrets/ssr-backend-url/url)}"
 export NUXT_SESSION_PASSWORD="${NUXT_SESSION_PASSWORD:=$(cat /etc/secrets/session-password/secret)}"
 export SENTRY_AUTH_TOKEN="${SENTRY_AUTH_TOKEN:=$(cat /etc/secrets/sentry-auth/token)}"
 export NUXT_PUBLIC_SENTRY_DSN="${NUXT_PUBLIC_SENTRY_DSN:=$(cat /etc/secrets/sentry-dsn/url)}"
