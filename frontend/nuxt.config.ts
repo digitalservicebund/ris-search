72f62b9f2365be35922155bad76709ec20fdca0b
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-10-15 11:39:26 +0200

    change comment on config to reflect actual behaviour

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -104,9 +104,9 @@ export default defineNuxtConfig({
     },
     /* Backend host to the spring backend, used by /proxy and /v1 routes  */
     risBackendUrl: "http://localhost:8090",
-    /* Backend URL to use when performing authenticated server-side rendering
-     (SSR), which might go to the Nuxt middleware at /api, or against the
-      backend directly. */
+    /* Backend host to use when performing authenticated server-side rendering
+     (SSR), which might go to the Nuxt middleware when empty, or to the
+      backend host provided. */
     ssrBackendUrl: "",
     session: {
       cookie: {
