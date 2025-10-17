a95512ec7b962ce9f7ae2fa211f4d6018c8d503d
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2025-10-17 15:12:15 +0200

    Add ticket number to list view test

--- frontend/e2e/translationsListView.spec.ts
+++ frontend/e2e/translationsListView.spec.ts
@@ -1,7 +1,7 @@
 import { test } from "@playwright/test";
 import { expect } from "./fixtures";
 
-test("opens the page via URL", async ({ page }) => {
+test("opens the page via URL", { tag: ["@RISDEV-8949"] }, async ({ page }) => {
   await page.goto("/translations");
   await expect(
     page.getByRole("heading", {
@@ -13,27 +13,35 @@ test("opens the page via URL", async ({ page }) => {
   );
 });
 
-test("displays search bar and list", async ({ page }) => {
-  await page.goto("/translations");
-  const searchInput = page.locator('input[type="search"]');
-  await expect(searchInput).toHaveCount(1);
+test(
+  "displays search bar and list",
+  { tag: ["@RISDEV-8949"] },
+  async ({ page }) => {
+    await page.goto("/translations");
+    const searchInput = page.locator('input[type="search"]');
+    await expect(searchInput).toHaveCount(1);
 
-  const links = page.locator('a[href^="translations"]');
-  await expect(links).toHaveCount(3);
-});
+    const links = page.locator('a[href^="translations"]');
+    await expect(links).toHaveCount(3);
+  },
+);
 
-test("filters translations correctly", async ({ page }) => {
-  await page.goto("/translations");
-  await page.waitForLoadState("networkidle");
-  const searchTerm = "Dentist";
+test(
+  "filters translations correctly",
+  { tag: ["@RISDEV-8949"] },
+  async ({ page }) => {
+    await page.goto("/translations");
+    await page.waitForLoadState("networkidle");
+    const searchTerm = "Dentist";
 
-  const input = page.getByPlaceholder("Enter search term");
-  await input.fill(searchTerm);
+    const input = page.getByPlaceholder("Enter search term");
+    await input.fill(searchTerm);
 
-  await expect(input).toHaveValue(searchTerm);
+    await expect(input).toHaveValue(searchTerm);
 
-  await input.press("Enter");
+    await input.press("Enter");
 
-  const links = page.locator('a[href^="translations"]');
-  await expect(links).toHaveCount(1);
-});
+    const links = page.locator('a[href^="translations"]');
+    await expect(links).toHaveCount(1);
+  },
+);
