4410a78f40fbba331f82bc3e010c547aa863e389
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-10-20 16:28:57 +0200

    sort props by temporalCoverageFrom

--- frontend/src/components/Norm/NormVersionWarning.vue
+++ frontend/src/components/Norm/NormVersionWarning.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import dayjs from "dayjs";
 import VersionWarningMessage from "~/components/Norm/VersionWarningMessage.vue";
 import type { LegislationWork, SearchResult } from "~/types";
 import {
@@ -32,7 +33,17 @@ const currentVersionValidityStatus = computed(() => {
 const firstFutureVersion = computed(() => {
   if (currentVersionValidityStatus.value !== "InForce") return undefined;
 
-  const firstFutureInForce = props.versions.find((v) => {
+  const sorted = props.versions.toSorted((v1, v2) => {
+    const from1 = temporalCoverageToValidityInterval(
+      v1.item.workExample.temporalCoverage,
+    )?.from;
+    const from2 = temporalCoverageToValidityInterval(
+      v2.item.workExample.temporalCoverage,
+    )?.from;
+    return dayjs(from1).diff(from2, "day");
+  });
+
+  const firstFutureInForce = sorted.find((v) => {
     return (
       getValidityStatus(
         temporalCoverageToValidityInterval(v.item.workExample.temporalCoverage),
