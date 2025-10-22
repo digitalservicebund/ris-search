755ab2e8fc6dbf74d65da867106137f1ddb729f6
ekl176 <eric.klemm@digitalservice.bund.de>
2025-10-22 12:42:56 +0200

    Risdev 9188 link to first future version (#1239)
    This PR changes the behavior of the message box in case a future version
    of a Fassung exists.
    Instead of referencing the latest future version of a Fassung, we want
    to display the next future Fassung.
    
    This should also solve the following issue:
    
    - in case a version with an undefined temporal coverage exists, we
    currently don't display the next future version, even though one exists

--- frontend/src/components/Norm/NormVersionWarning.vue
+++ frontend/src/components/Norm/NormVersionWarning.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import dayjs from "dayjs";
 import VersionWarningMessage from "~/components/Norm/VersionWarningMessage.vue";
 import type { LegislationWork, SearchResult } from "~/types";
 import {
@@ -29,15 +30,28 @@ const currentVersionValidityStatus = computed(() => {
   return getValidityStatus(validityInterval);
 });
 
-const latestFutureVersion = computed(() => {
+const firstFutureVersion = computed(() => {
   if (currentVersionValidityStatus.value !== "InForce") return undefined;
-  const last = props.versions[props.versions.length - 1];
-  const latestValidityInterval = temporalCoverageToValidityInterval(
-    last.item.workExample.temporalCoverage,
-  );
-  return getValidityStatus(latestValidityInterval) === "FutureInForce"
-    ? last.item
-    : undefined;
+
+  const sorted = props.versions.toSorted((v1, v2) => {
+    const from1 = temporalCoverageToValidityInterval(
+      v1.item.workExample.temporalCoverage,
+    )?.from;
+    const from2 = temporalCoverageToValidityInterval(
+      v2.item.workExample.temporalCoverage,
+    )?.from;
+    return dayjs(from1).diff(from2, "day");
+  });
+
+  const firstFutureInForce = sorted.find((v) => {
+    return (
+      getValidityStatus(
+        temporalCoverageToValidityInterval(v.item.workExample.temporalCoverage),
+      ) === "FutureInForce"
+    );
+  });
+
+  return firstFutureInForce?.item;
 });
 </script>
 
@@ -45,7 +59,7 @@ const latestFutureVersion = computed(() => {
   <VersionWarningMessage
     :current-version-validity-status="currentVersionValidityStatus"
     :in-force-version-link="inForceVersionLink"
-    :future-version="latestFutureVersion"
+    :future-version="firstFutureVersion"
     historical-warning-message="Historische Fassung."
     future-warning-message="Zukünftige Fassung."
   />
