cdd8accd652f29ebb35779e07f9bd1a9172240a2
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-10-22 16:30:16 +0200

    use caffeine cache to manage request count

--- backend/src/main/java/de/bund/digitalservice/ris/search/config/ratelimiting/RateLimitInterceptor.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/config/ratelimiting/RateLimitInterceptor.java
@@ -1,20 +1,18 @@
 package de.bund.digitalservice.ris.search.config.ratelimiting;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public abstract class RateLimitInterceptor implements HandlerInterceptor {
-  private final Map<String, AtomicInteger> requestCountsPerIpAddress = new ConcurrentHashMap<>();
-  private final Map<String, Long> requestTimestamps = new ConcurrentHashMap<>();
+  Cache<String, Integer> requestCountsPerIpAddress =
+      Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES).build();
   private final int maxRequestsPerMinute;
-  private static final long TIME_LIMIT = 60000;
 
   public RateLimitInterceptor(int maxRequestsPerMinute) {
     this.maxRequestsPerMinute = maxRequestsPerMinute;
@@ -25,32 +23,17 @@ public abstract class RateLimitInterceptor implements HandlerInterceptor {
       throws IOException {
 
     String clientIpAddress = request.getRemoteAddr();
-    long currentTime = System.currentTimeMillis();
 
-    AtomicInteger requestCount =
-        requestCountsPerIpAddress.computeIfAbsent(clientIpAddress, k -> new AtomicInteger(0));
-    long firstRequestTime = requestTimestamps.computeIfAbsent(clientIpAddress, k -> currentTime);
-
-    long timeElapsed = calculateTimeElapsed(firstRequestTime, currentTime);
-
-    // Reset if more than 1 minute has passed
-    if (timeElapsed > TIME_LIMIT) {
-      requestCount.set(0);
-      requestTimestamps.put(clientIpAddress, currentTime);
+    Integer requestCount = requestCountsPerIpAddress.getIfPresent(clientIpAddress);
+    if (Objects.isNull(requestCount)) {
+      requestCount = 1;
     }
 
-    int requests = requestCount.incrementAndGet();
-    if (requests > maxRequestsPerMinute) {
+    if (requestCount > maxRequestsPerMinute) {
       response.sendError(429);
       return false;
     }
-
+    requestCountsPerIpAddress.put(clientIpAddress, ++requestCount);
     return true;
-  }
-
-  private long calculateTimeElapsed(long firstRequestTime, long currentTime) {
-    return Duration.between(
-            Instant.ofEpochMilli(firstRequestTime), Instant.ofEpochMilli(currentTime))
-        .toMillis();
   }
 }
