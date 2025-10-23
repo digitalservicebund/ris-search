23b0478e90aa76f2031048c925c793fa2490adb3
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-10-23 15:32:31 +0200

    add default rate limiter

--- backend/src/main/java/de/bund/digitalservice/ris/search/config/WebMvcConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package de.bund.digitalservice.ris.search.config;
 
+import de.bund.digitalservice.ris.search.config.ratelimiting.DefaultRateLimitInterceptor;
 import de.bund.digitalservice.ris.search.config.ratelimiting.FeedbackRateLimitInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -10,13 +11,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
   private final FeedbackRateLimitInterceptor feedbackInterceptor;
 
-  WebMvcConfig(FeedbackRateLimitInterceptor interceptor) {
+  private final DefaultRateLimitInterceptor defaultRateLimitInterceptor;
+
+  WebMvcConfig(
+      FeedbackRateLimitInterceptor interceptor,
+      DefaultRateLimitInterceptor defaultRateLimitInterceptor) {
+
     this.feedbackInterceptor = interceptor;
+    this.defaultRateLimitInterceptor = defaultRateLimitInterceptor;
   }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(feedbackInterceptor).addPathPatterns(ApiConfig.Paths.FEEDBACK);
-    // registry.addInterceptor("/v1")
+    registry.addInterceptor(defaultRateLimitInterceptor);
   }
 }
