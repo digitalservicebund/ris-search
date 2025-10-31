5580dd47a0ec64d64a0b03225827485f7b3d7b0d
Haytham Mones <haythameyd@gmail.com>
2025-10-31 13:50:46 +0100

    Fixed navigation top bar in staging and prototype

--- frontend/src/layouts/base.vue
+++ frontend/src/layouts/base.vue
@@ -3,10 +3,6 @@ import { NuxtLoadingIndicator } from "#components";
 import ConsentBanner from "~/components/Analytics/ConsentBanner.vue";
 import AppFooter from "~/components/AppFooter.vue";
 import AppHeader from "~/components/AppHeader.vue";
-import NavbarTop from "~/components/NavbarTop.vue";
-import { usePrivateFeaturesFlag } from "~/composables/usePrivateFeaturesFlag";
-
-const privateFeaturesEnabled = usePrivateFeaturesFlag();
 </script>
 
 <template>
@@ -16,8 +12,7 @@ const privateFeaturesEnabled = usePrivateFeaturesFlag();
   </client-only>
   <div class="flex flex-col gap-48">
     <div class="min-h-[50vh] bg-gray-100">
-      <AppHeader v-if="privateFeaturesEnabled" />
-      <NavbarTop v-else />
+      <AppHeader />
       <main>
         <slot />
       </main>
