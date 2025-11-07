d8045f93de89f8d7504a42ebf125becfae6954d2
Haytham Mones <haythameyd@gmail.com>
2025-11-07 10:07:46 +0100

    Disabled Screenshot saving and uploading to ris-reports

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -95,7 +95,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: environment.baseUrl,
-    screenshot: { mode: "on", fullPage: true },
+    screenshot: { mode: "only-on-failure", fullPage: true },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-first-failure",
     extraHTTPHeaders,
