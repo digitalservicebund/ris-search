aa40869dc98f88dd02c59469d136b20c44314676
Haytham Mones <haythameyd@gmail.com>
2025-11-11 22:04:54 +0100

    Changed path in feedback and robots.txt to use the composable

--- frontend/src/server/routes/robots.txt.get.ts
+++ frontend/src/server/routes/robots.txt.get.ts
@@ -1,9 +1,9 @@
 import { getRequestURL, getHeader, setHeader, defineEventHandler } from "h3";
+import { useBackendURL } from "~/composables/useBackendURL";
 import { usePrivateFeaturesFlag } from "~/composables/usePrivateFeaturesFlag";
 
 export default defineEventHandler(async (event) => {
   const userAgent = (getHeader(event, "User-Agent") ?? "").toUpperCase();
-  const config = useRuntimeConfig();
   const privateFeaturesEnabled = usePrivateFeaturesFlag();
   let file = privateFeaturesEnabled
     ? "robots.staging.txt"
@@ -15,7 +15,7 @@ export default defineEventHandler(async (event) => {
   const { origin } = getRequestURL(event);
 
   if (userAgent === "DG_JUSTICE_CRAWLER") {
-    const backendUrl = config.risBackendUrl;
+    const backendUrl = useBackendURL();
     return await $fetch<string>(`${backendUrl}/v1/eclicrawler/robots.txt`, {
       method: "GET",
     });
