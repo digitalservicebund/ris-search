db54ddfc4ae0e590578d4d8bfca716b97350a6fa
Haytham Mones <haythameyd@gmail.com>
2025-11-12 15:39:02 +0100

    Fixed e2e pipeline

--- frontend/src/composables/useBackendUrl.ts
+++ frontend/src/composables/useBackendUrl.ts
@@ -1,6 +1,6 @@
 /**
  * A custom composable that receives a url and return it prepended with
- * the backend base URL
+ * the backend base URL. The function returns the backend url when no input
  *
  * @param url An optional path that needs to be prepended (e.g., '/v1/legislation' => 'http://localhost:8090/v1/legislation').
  * @returns A string with the full path.
