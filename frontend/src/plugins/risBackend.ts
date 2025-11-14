f598c4aea3ac62f3872be54fd734c635dab4a427
Haytham Mones <haythameyd@gmail.com>
2025-11-14 03:00:56 +0100

    Fixed issue with auth headers not added to client side calls

--- frontend/src/plugins/risBackend.ts
+++ frontend/src/plugins/risBackend.ts
@@ -1,12 +1,9 @@
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig();
-  const basicAuth = config.basicAuth;
+  const basicAuth = useRuntimeConfig().basicAuth;
 
   const risBackend = $fetch.create({
     onRequest({ options }) {
-      if (import.meta.server && basicAuth) {
-        options.headers.set("Authorization", `Basic ${basicAuth}`);
-      }
+      options.headers.set("Authorization", `Basic ${basicAuth}`);
     },
   });
 
