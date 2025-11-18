d910304e03b3920487e6159989e7b570970e1b5c
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-11-18 15:27:55 +0100

    ci: temporarily capture more detailed traces
    RISDEV-0000

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -60,9 +60,9 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: environment.baseUrl,
-    screenshot: { mode: "only-on-failure", fullPage: true },
+    screenshot: { mode: "on", fullPage: true },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "retain-on-first-failure",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
