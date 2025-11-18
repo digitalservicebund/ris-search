a269a81ea19a45b224457897fdb0b10bf37b17bf
andrew.cenkner <andrew.cenkner@digitalservice.bund.de>
2025-11-18 08:55:17 +0100

    RISDEV-9869
    Indexing R+N | Add missing fields to index
    *Fix merge conflict

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/controller/api/NormsControllerApiTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/controller/api/NormsControllerApiTest.java
@@ -44,6 +44,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
