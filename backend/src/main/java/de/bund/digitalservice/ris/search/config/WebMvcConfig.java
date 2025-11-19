d2dc8b2f3434b780616aa4d1f2254568d5420f64
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2025-11-19 16:13:31 +0100

    Move CORS configuration to WebMvcConfig

--- backend/src/main/java/de/bund/digitalservice/ris/search/config/WebMvcConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/config/WebMvcConfig.java
@@ -2,12 +2,23 @@ package de.bund.digitalservice.ris.search.config;
 
 import de.bund.digitalservice.ris.search.config.ratelimiting.DefaultRateLimitInterceptor;
 import de.bund.digitalservice.ris.search.config.ratelimiting.FeedbackRateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+  @Value("${server.front-end-url}")
+  String frontEndUrl;
+
+  @Value("${server.docs-url}")
+  String docsUrl;
 
   private final FeedbackRateLimitInterceptor feedbackInterceptor;
 
@@ -25,5 +36,46 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(feedbackInterceptor).addPathPatterns(ApiConfig.Paths.FEEDBACK);
     registry.addInterceptor(defaultRateLimitInterceptor);
+  }
+
+  /**
+   * Method to configure CORS for private applications.
+   *
+   * @return The web mvc configurer
+   */
+  @Bean
+  public WebMvcConfigurer privateCorsConfigurer() {
+
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods(CorsConfiguration.ALL)
+            .allowedHeaders(CorsConfiguration.ALL)
+            .allowedOrigins(frontEndUrl, docsUrl);
+      }
+    };
+  }
+
+  /**
+   * Method to configure CORS for public application and E2E tests.
+   *
+   * @return The web mvc configurer
+   */
+  @Bean
+  @Profile({"e2e", "prototype"})
+  public WebMvcConfigurer publicCorsConfigurer() {
+
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+            .addMapping("/**")
+            .allowedMethods(CorsConfiguration.ALL)
+            .allowedHeaders(CorsConfiguration.ALL)
+            .allowedOrigins(CorsConfiguration.ALL);
+      }
+    };
   }
 }
