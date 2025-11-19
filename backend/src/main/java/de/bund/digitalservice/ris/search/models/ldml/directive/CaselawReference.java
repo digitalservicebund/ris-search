6630062a89a7947742b85d3627f0d4ceb759b214
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-11-19 10:51:30 +0100

    remove obsolete attributes from schemas

--- backend/src/main/java/de/bund/digitalservice/ris/search/models/ldml/directive/CaselawReference.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/models/ldml/directive/CaselawReference.java
@@ -1,7 +1,6 @@
 package de.bund.digitalservice.ris.search.models.ldml.directive;
 
 import jakarta.xml.bind.annotation.XmlAttribute;
-import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,18 +8,5 @@ import lombok.Setter;
 @Setter
 public class CaselawReference {
 
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private String court;
-
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private LocalDate date;
-
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private String referenceNumber;
-
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private String documentNumber;
-
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private String courtLocation;
+  @XmlAttribute private String referenceNumber;
 }
--- backend/src/main/java/de/bund/digitalservice/ris/search/models/ldml/directive/NormReference.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/models/ldml/directive/NormReference.java
@@ -1,7 +1,6 @@
 package de.bund.digitalservice.ris.search.models.ldml.directive;
 
 import jakarta.xml.bind.annotation.XmlAttribute;
-import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +8,5 @@ import lombok.Setter;
 @Setter
 public class NormReference {
 
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private LocalDate dateOfRelevance;
-
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private LocalDate dateOfVersion;
-
-  @XmlAttribute(namespace = AdministrativeDirectiveLdml.RIS_NS)
-  private String singleNorm;
+  @XmlAttribute private String singleNorm;
 }
