c436c3c43e65ca752350c39263b5836922612d05
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2025-11-19 16:23:17 +0100

    Allow swagger

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/CSPHeadersIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/CSPHeadersIntegrationTest.java
@@ -4,7 +4,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 
 import de.bund.digitalservice.ris.search.integration.config.ContainersIntegrationBase;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,14 +12,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@Tag("integration")
 public class CSPHeadersIntegrationTest extends ContainersIntegrationBase {
 
   @Autowired private MockMvc mockMvc;
 
   private final String CSP_HEADER_NAME = "Content-Security-Policy";
   private final String CSP_HEADER_VALUE =
-      "default-src 'none'; script-src 'none'; style-src 'unsafe-inline'; img-src data:; connect-src 'none'; font-src 'none'; frame-src 'none'; object-src 'none'; media-src 'none';";
+      "default-src 'none'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src data: 'self'; connect-src 'self'; font-src 'self'; frame-src 'none'; object-src 'none'; media-src 'none';";
 
   @Test
   void shouldExposeEndpointsWithCSP() throws Exception {
