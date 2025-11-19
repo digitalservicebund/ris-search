174e75090dde27476b7ef5e02512182dbbbc46bf
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2025-11-19 16:34:26 +0100

    Fix sonarqube issues

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/CSPHeadersIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/CSPHeadersIntegrationTest.java
@@ -12,33 +12,31 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-public class CSPHeadersIntegrationTest extends ContainersIntegrationBase {
+class CSPHeadersIntegrationTest extends ContainersIntegrationBase {
 
   @Autowired private MockMvc mockMvc;
 
-  private final String CSP_HEADER_NAME = "Content-Security-Policy";
-  private final String CSP_HEADER_VALUE =
+  private final String cspHeaderName = "Content-Security-Policy";
+  private final String cspHeaderValue =
       "default-src 'none'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src data: 'self'; connect-src 'self'; font-src 'self'; frame-src 'none'; object-src 'none'; media-src 'none';";
 
   @Test
   void shouldExposeEndpointsWithCSP() throws Exception {
     mockMvc
         .perform(get("/v1/legislation"))
-        .andExpect(header().string(CSP_HEADER_NAME, CSP_HEADER_VALUE));
-    mockMvc
-        .perform(get("/v1/case-law"))
-        .andExpect(header().string(CSP_HEADER_NAME, CSP_HEADER_VALUE));
+        .andExpect(header().string(cspHeaderName, cspHeaderValue));
+    mockMvc.perform(get("/v1/case-law")).andExpect(header().string(cspHeaderName, cspHeaderValue));
     mockMvc
         .perform(get("/v1/literature"))
-        .andExpect(header().string(CSP_HEADER_NAME, CSP_HEADER_VALUE));
+        .andExpect(header().string(cspHeaderName, cspHeaderValue));
     mockMvc
         .perform(get("/v1/literature/XXLU000000001.akn.xml"))
-        .andExpect(header().string(CSP_HEADER_NAME, CSP_HEADER_VALUE));
+        .andExpect(header().string(cspHeaderName, cspHeaderValue));
     mockMvc
         .perform(get("/v1/literature/XXLU000000001.html"))
-        .andExpect(header().string(CSP_HEADER_NAME, CSP_HEADER_VALUE));
+        .andExpect(header().string(cspHeaderName, cspHeaderValue));
     mockMvc
         .perform(get("/v1/statistics"))
-        .andExpect(header().string(CSP_HEADER_NAME, CSP_HEADER_VALUE));
+        .andExpect(header().string(cspHeaderName, cspHeaderValue));
   }
 }
