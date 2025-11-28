d570fe3962fe9128a6a164278784c87a58defab6
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-11-28 15:59:28 +0100

    serve sitemaps from server

--- frontend/src/server/routes/robots.txt.get.ts
+++ frontend/src/server/routes/robots.txt.get.ts
@@ -1,18 +1,13 @@
-import { getRequestURL, getHeader, setHeader, defineEventHandler } from "h3";
+import { getHeader, setHeader, defineEventHandler } from "h3";
 import useBackendUrl from "~/composables/useBackendUrl";
 import { usePrivateFeaturesFlag } from "~/composables/usePrivateFeaturesFlag";
 
 export default defineEventHandler(async (event) => {
   const userAgent = (getHeader(event, "User-Agent") ?? "").toUpperCase();
   const privateFeaturesEnabled = usePrivateFeaturesFlag();
-  let file = privateFeaturesEnabled
-    ? "robots.staging.txt"
-    : "robots.public.txt";
-  if (userAgent === "DG_JUSTICE_CRAWLER") file = "robots.dg.txt";
 
   setHeader(event, "Content-Type", "text/plain; charset=utf-8");
   setHeader(event, "Vary", "User-Agent");
-  const { origin } = getRequestURL(event);
 
   if (userAgent === "DG_JUSTICE_CRAWLER") {
     return await $fetch<string>(useBackendUrl(`/v1/eclicrawler/robots.txt`), {
@@ -20,6 +15,10 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const url = `${origin}/${file}`;
-  return await $fetch<string>(url, { method: "GET" });
+  const storage = useStorage("assets:server");
+  if (privateFeaturesEnabled) {
+    return storage.getItem("robots.staging.txt");
+  } else {
+    return storage.getItem("robots.public.txt");
+  }
 });
