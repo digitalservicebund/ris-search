d6e9beb46dbbe695bfbcaa5730b15db66b119a0d
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-12-02 11:57:35 +0100

    fix: add note about blank target to external links
    RISDEV-9456

--- frontend/src/components/ExternalLink.spec.ts
+++ frontend/src/components/ExternalLink.spec.ts
@@ -7,14 +7,18 @@ describe("ExternalLink", () => {
   it("renders external link", async () => {
     await renderSuspended(ExternalLink, {
       props: { url: "https://example.com" },
-      slots: { default: "Example" },
+      slots: { default: () => "Example" },
     });
 
-    const link = screen.getByRole("link", { name: "Example" });
+    const link = screen.getByRole("link", {
+      name: "Example (öffnet in einem neuen Tab)",
+    });
+
     expect(link).toHaveAttribute(
       "href",
       expect.stringContaining("https://example.com"),
     );
+
     expect(link).toHaveAttribute("target", "_blank");
   });
 });
