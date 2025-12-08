dafb13f5690ad3b163e6ced8822ff5f657fec4aa
UrsKahmann <33418462+UrsKahmann@users.noreply.github.com>
2025-12-08 13:26:31 +0100

    RISDEV-9032: Add VV actions menu (#1469)
    RISDEV-9032

---
+++ frontend/src/components/ActionMenu/AdministrativeDirectiveActionsMenu.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ActionsMenu from "~/components/ActionMenu/ActionsMenu.vue";
+import type { AdministrativeDirective } from "~/types";
+
+const { administrativeDirective } = defineProps<{
+  administrativeDirective: AdministrativeDirective | undefined;
+}>();
+
+const xmlUrl = computed(() => {
+  const encoding = administrativeDirective?.encoding?.find(
+    (e) => e.encodingFormat === "application/xml",
+  );
+  return encoding?.contentUrl ?? undefined;
+});
+
+const permalink = {
+  label: "Link kopieren",
+  url: globalThis?.location?.href,
+};
+</script>
+
+<template>
+  <ActionsMenu :permalink="permalink" :xml-url="xmlUrl" />
+</template>
