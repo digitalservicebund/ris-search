99c5eee288ef06de8b40e1e0d25d0826739ac66d
Eric Klemm <eric.klemm@digitalservice.bund.de>
2025-12-10 15:59:30 +0100

    add missing javadoc

--- backend/src/main/java/de/bund/digitalservice/ris/search/mapper/LiteratureTypeMapper.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/mapper/LiteratureTypeMapper.java
@@ -2,9 +2,16 @@ package de.bund.digitalservice.ris.search.mapper;
 
 import de.bund.digitalservice.ris.search.models.ldml.literature.LiteratureType;
 
+/** Maps a LiteratureType to its api representation */
 public class LiteratureTypeMapper {
   private LiteratureTypeMapper() {}
 
+  /**
+   * Maps a LiteratureType to its api representation
+   *
+   * @param documentNumber String the type is determined by its document number
+   * @return Api representation of a literature type
+   */
   public static String mapLiteratureType(String documentNumber) {
     return switch (LiteratureType.getByDocumentNumber(documentNumber)) {
       case SLI -> "sli";
