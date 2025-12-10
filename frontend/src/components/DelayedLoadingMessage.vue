e177293fc7a1702096dafa6270f5bffe50a6d466
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2025-12-10 12:28:10 +0100

    refactor: remove redundant spinner component
    RISDEV-9450

--- frontend/src/components/DelayedLoadingMessage.vue
+++ frontend/src/components/DelayedLoadingMessage.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ProgressSpinner } from "primevue";
+</script>
 
 <template>
   <div
     class="delay-visibility m-y-10 top-0 right-0 bottom-0 left-0 grid justify-items-center gap-y-8 p-10"
   >
-    <LoadingSpinner />
+    <ProgressSpinner />
     <slot></slot>
   </div>
 </template>
