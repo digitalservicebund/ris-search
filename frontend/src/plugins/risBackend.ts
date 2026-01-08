919f896378a642d20edcb3c3969a38468960efd5
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-01-08 13:18:33 +0100

    Access server side property only on server side
    - fixes a warning about accessing the property "basicAuth" on the client which is only available on the server side

--- frontend/src/plugins/risBackend.ts
+++ frontend/src/plugins/risBackend.ts
@@ -1,13 +1,15 @@
 export default defineNuxtPlugin(() => {
   const config = useRuntimeConfig();
-  const basicAuth = config.basicAuth;
   const backendUrl = config.public.risBackendUrl;
 
   const risBackend = $fetch.create({
     baseURL: backendUrl,
     onRequest({ options }) {
-      if (import.meta.server && basicAuth) {
-        options.headers.set("Authorization", `Basic ${basicAuth}`);
+      if (import.meta.server) {
+        const basicAuth = config.basicAuth;
+        if (basicAuth) {
+          options.headers.set("Authorization", `Basic ${basicAuth}`);
+        }
       }
     },
   });
