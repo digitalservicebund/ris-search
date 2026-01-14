4b886105b68984c5271720bed784ce06e928ccd8
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2026-01-14 12:05:37 +0100

    feat: add custom renderer for links on Markdown pages
    RISDEV-10515

---
+++ frontend/src/components/prose/ProseA.global.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const { href } = defineProps<{
+  href: string;
+  target?: string;
+}>();
+
+const isExternal = computed(() => href.match(/^https?:\/\//));
+</script>
+
+<template>
+  <ExternalLink v-if="isExternal" :url="href">
+    <slot />
+  </ExternalLink>
+
+  <NuxtLink v-else :href :target>
+    <slot />
+  </NuxtLink>
+</template>
