468b676af127bf2c0e10e2686b2f6aca4d825130
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2026-01-15 17:30:48 +0100

    Ignore Task Processor in sonar

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -44,7 +44,10 @@ sonar {
         property("sonar.projectKey", "digitalservicebund_ris-search-backend")
         property("sonar.organization", "digitalservicebund")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.exclusions", "**/config/**, **/e2e/**, **/CustomErrorController.java, **/RestClientConfigStackit.java")
+        property(
+            "sonar.coverage.exclusions",
+            "**/config/**, **/e2e/**, **/CustomErrorController.java, **/RestClientConfigStackit.java, **/ImportTaskProcessorProduction.java",
+        )
         property("sonar.cpd.exclusions", "**/RestClientConfigStackit.java, **/ImportTaskProcessorProduction.java")
     }
 }
