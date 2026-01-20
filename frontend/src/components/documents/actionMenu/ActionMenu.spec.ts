1337d4be67966c764cf0f68bdf21f25720b5d6e1
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-01-20 14:41:28 +0100

    Add test for action menu
    RISDEV-10305

---
+++ frontend/src/components/documents/actionMenu/ActionMenu.spec.ts
@@ -0,0 +1,118 @@
+import userEvent from "@testing-library/user-event/dist/cjs/index.js";
+import { render, screen } from "@testing-library/vue";
+import Tooltip from "primevue/tooltip";
+import { vi } from "vitest";
+import ActionMenu, {
+  type ActionMenuItem,
+} from "~/components/documents/actionMenu/ActionMenu.vue";
+
+const mockCommand = vi.fn();
+const actions: ActionMenuItem[] = [
+  {
+    label: "Command Action",
+    iconComponent: h("span", "CommandIcon"),
+    command: mockCommand,
+  },
+  {
+    label: "Navigate Action",
+    url: "https://example.com",
+    iconComponent: h("span", "NavigateIcon"),
+  },
+];
+
+describe("ActionMenu", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("renders provided action items", async () => {
+    const user = userEvent.setup();
+
+    render(ActionMenu, {
+      props: {
+        actions: actions,
+      },
+      global: {
+        directives: { tooltip: Tooltip },
+        stubs: {
+          NuxtLink: {
+            template: '<a :href="to"><slot /></a>',
+            props: ["to"],
+          },
+        },
+      },
+    });
+
+    // Command actions are rendered as buttons
+    const commandActionButton = screen.getByRole("button", {
+      name: "Command Action",
+    });
+
+    expect(commandActionButton).toBeVisible();
+    expect(commandActionButton).not.toBeDisabled();
+
+    // Displays the provided icon component
+    expect(screen.getByText("CommandIcon")).toBeVisible();
+
+    // Shows tooltip when hovered
+    await user.hover(commandActionButton);
+    expect(await screen.findByRole("tooltip")).toHaveTextContent(
+      "Command Action",
+    );
+
+    // Executes provided command when clicked
+    expect(mockCommand).not.toHaveBeenCalled();
+    await user.click(commandActionButton);
+    expect(mockCommand).toHaveBeenCalledOnce();
+
+    // navigate actions are rendered as links
+    const navigateActionLink = screen.getByRole("link", {
+      name: "Navigate Action",
+    });
+
+    expect(navigateActionLink).toBeVisible();
+    expect(navigateActionLink).not.toBeDisabled();
+    expect(navigateActionLink).toHaveAttribute("href", "https://example.com");
+
+    // Displays the provided icon component
+    expect(screen.getByText("NavigateIcon")).toBeVisible();
+
+    // Shows tooltip when hovered
+    await user.hover(navigateActionLink);
+    expect(await screen.findByRole("tooltip")).toHaveTextContent(
+      "Navigate Action",
+    );
+  });
+
+  it("renders disabled action items", async () => {
+    const user = userEvent.setup();
+
+    render(ActionMenu, {
+      props: {
+        actions: [
+          {
+            label: "Disabled Action",
+            iconComponent: h("span", "DisabledIcon"),
+            command: mockCommand,
+            disabled: true,
+          },
+        ],
+      },
+      global: {
+        directives: { tooltip: Tooltip },
+      },
+    });
+
+    const disabledButton = screen.getByRole("button", {
+      name: "Disabled Action",
+    });
+
+    expect(disabledButton).toBeVisible();
+    expect(disabledButton).toBeDisabled();
+
+    // Executes provided command when clicked
+    expect(mockCommand).not.toHaveBeenCalled();
+    await user.click(disabledButton);
+    expect(mockCommand).not.toHaveBeenCalled();
+  });
+});
