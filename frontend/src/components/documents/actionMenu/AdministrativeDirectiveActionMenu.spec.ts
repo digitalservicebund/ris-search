4860a27a4ee4a9a9598e7ae8282b1dce95410492
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-01-22 13:31:30 +0100

    Refactor AdministrativeDirectiveActionMenu
    RISDEV-10305

--- frontend/src/components/documents/actionMenu/AdministrativeDirectiveActionMenu.spec.ts
+++ frontend/src/components/documents/actionMenu/AdministrativeDirectiveActionMenu.spec.ts
@@ -1,37 +1,137 @@
-import { shallowMount } from "@vue/test-utils";
+import { mockNuxtImport } from "@nuxt/test-utils/runtime";
+import { userEvent } from "@testing-library/user-event/dist/cjs/index.js";
+import { render, screen } from "@testing-library/vue";
+import Tooltip from "primevue/tooltip";
 import { describe, expect, it, vi } from "vitest";
-import ActionMenuWrapper from "~/components/documents/actionMenu/ActionMenuWrapper.vue";
 import AdministrativeDirectiveActionMenu from "~/components/documents/actionMenu/AdministrativeDirectiveActionMenu.vue";
 import type { AdministrativeDirective } from "~/types";
 
-describe("AdministrativeDirectiveActionMenu", () => {
-  it("passes correct props to ActionMenu", () => {
-    vi.stubGlobal("location", {
-      href: "https://test.com",
-    });
+const { mockToastAdd } = vi.hoisted(() => ({
+  mockToastAdd: vi.fn(),
+}));
 
-    const mockedAdministrativeDirective = {
-      encoding: [
-        {
-          contentUrl: "/administrative-directive/XXLU000000001.xml",
-          encodingFormat: "application/xml",
+vi.mock("~/composables/useBackendUrl", () => ({
+  default: vi.fn((url?: string) => url),
+}));
+
+vi.mock("primevue/usetoast", () => ({
+  useToast: () => ({
+    add: mockToastAdd,
+  }),
+}));
+
+mockNuxtImport("useRequestURL", () => {
+  return () => ({
+    href: "https://example.com/administrative-directive",
+  });
+});
+
+const mockedAdministrativeDirective = {
+  encoding: [
+    {
+      contentUrl:
+        "https://example.com/v1/administrative-directive/XXAD000000001.xml",
+      encodingFormat: "application/xml",
+    },
+  ],
+} as AdministrativeDirective;
+
+function renderAdministrativeDirectiveActionMenu() {
+  render(AdministrativeDirectiveActionMenu, {
+    props: {
+      administrativeDirective: mockedAdministrativeDirective,
+    },
+    global: {
+      directives: { tooltip: Tooltip },
+      stubs: {
+        NuxtLink: {
+          template: '<a :href="to"><slot /></a>',
+          props: ["to"],
         },
-      ],
-    } as AdministrativeDirective;
-
-    const wrapper = shallowMount(AdministrativeDirectiveActionMenu, {
-      props: {
-        administrativeDirective: mockedAdministrativeDirective,
       },
-    });
+    },
+  });
+}
 
-    const actionMenu = wrapper.findComponent(ActionMenuWrapper);
-    expect(actionMenu.props()).toMatchObject({
-      permalink: {
-        label: "Link kopieren",
-        url: "https://test.com",
-      },
-      xmlUrl: "/administrative-directive/XXLU000000001.xml",
+// NOTE: only testing the "desktop" variant here as testing the different
+// variants which are based on the screen size is not reliably doable
+// without a real browser - desktop and mobile will be tested in the e2e tests
+describe("LiteratureActionMenu", () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("renders all actions in correct order", async () => {
+    renderAdministrativeDirectiveActionMenu();
+    const menuitems = await screen.findAllByRole("menuitem");
+
+    expect(menuitems).toHaveLength(4);
+    expect(menuitems[0]).toHaveAccessibleName("Link kopieren");
+    expect(menuitems[1]).toHaveAccessibleName("Drucken");
+    expect(menuitems[2]).toHaveAccessibleName("Als PDF speichern");
+    expect(menuitems[3]).toHaveAccessibleName("XML anzeigen");
+  });
+
+  it("can copy link to currently viewed document", async () => {
+    const user = userEvent.setup();
+    renderAdministrativeDirectiveActionMenu();
+
+    const copyButton = screen.getByRole("menuitem", {
+      name: "Link kopieren",
     });
+    expect(copyButton).toBeVisible();
+    expect(copyButton).toBeEnabled();
+
+    await user.click(copyButton);
+
+    expect(await navigator.clipboard.readText()).toEqual(
+      "https://example.com/administrative-directive",
+    );
+
+    expect(mockToastAdd).toHaveBeenCalledExactlyOnceWith(
+      expect.objectContaining({
+        summary: "Kopiert!",
+      }),
+    );
+  });
+
+  it("can open the print dialog", async () => {
+    globalThis.print = vi.fn();
+    const user = userEvent.setup();
+    renderAdministrativeDirectiveActionMenu();
+
+    const printButton = screen.getByRole("menuitem", {
+      name: "Drucken",
+    });
+    expect(printButton).toBeVisible();
+    expect(printButton).toBeEnabled();
+
+    await user.click(printButton);
+
+    expect(globalThis.print).toHaveBeenCalledOnce();
+  });
+
+  it("renders disabled PDF button", async () => {
+    renderAdministrativeDirectiveActionMenu();
+
+    const pdfButton = screen.getByRole("menuitem", {
+      name: "Als PDF speichern",
+    });
+    expect(pdfButton).toBeVisible();
+    expect(pdfButton).toBeDisabled();
+  });
+
+  it("can open link to xml view", async () => {
+    renderAdministrativeDirectiveActionMenu();
+
+    const xmlLink = screen.getByRole("menuitem", {
+      name: "XML anzeigen",
+    });
+    expect(xmlLink).toBeVisible();
+    expect(xmlLink).toBeEnabled();
+    expect(xmlLink).toHaveAttribute(
+      "href",
+      "https://example.com/v1/administrative-directive/XXAD000000001.xml",
+    );
   });
 });
--- frontend/src/components/documents/actionMenu/AdministrativeDirectiveActionMenu.vue
+++ frontend/src/components/documents/actionMenu/AdministrativeDirectiveActionMenu.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
-import ActionMenuWrapper from "~/components/documents/actionMenu/ActionMenuWrapper.vue";
+import ActionMenu from "~/components/documents/actionMenu/ActionMenu.vue";
+import { useCopyUrlActionItem } from "~/composables/useActionMenuItem/useCopyUrlActionItem";
+import { usePdfActionItem } from "~/composables/useActionMenuItem/usePdfActionItem";
+import { usePrintActionItem } from "~/composables/useActionMenuItem/usePrintActionItem";
+import { useXmlActionItem } from "~/composables/useActionMenuItem/useXmlActionItem";
 import type { AdministrativeDirective } from "~/types";
 
 const { administrativeDirective } = defineProps<{
   administrativeDirective: AdministrativeDirective | undefined;
 }>();
 
-const xmlUrl = computed(() => {
-  const encoding = administrativeDirective?.encoding?.find(
-    (e) => e.encodingFormat === "application/xml",
-  );
-  return encoding?.contentUrl ?? undefined;
-});
+const actions = computed(() => {
+  const permalink = useRequestURL().href;
 
-const permalink = {
-  label: "Link kopieren",
-  url: globalThis?.location?.href,
-};
+  const xmlUrl = useBackendUrl(
+    administrativeDirective?.encoding?.find(
+      (e) => e.encodingFormat === "application/xml",
+    )?.contentUrl ?? undefined,
+  );
+
+  return [
+    useCopyUrlActionItem(permalink),
+    usePrintActionItem(),
+    usePdfActionItem(),
+    useXmlActionItem(xmlUrl),
+  ];
+});
 </script>
 
 <template>
-  <ActionMenuWrapper :permalink="permalink" :xml-url="xmlUrl" />
+  <ActionMenu :actions />
 </template>
