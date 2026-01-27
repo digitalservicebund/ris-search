8d08ac2c989079032f9509368c59279c0a0303f8
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2026-01-27 07:21:38 +0100

    chore: add test page for Sentry integration
    RISDEV-9936

---
+++ frontend/src/pages/sentry-test.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from "primevue";
+
+function throwError() {
+  throw new Error("testing Sentry integration");
+}
+</script>
+
+<template>
+  <Button label="Throw error" @click="throwError()" />
+</template>
