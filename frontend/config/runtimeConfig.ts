0b777f39e1c1c0f2aa1af484a254aa73d02ca1ed
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2026-01-28 14:14:03 +0100

    Try to read secret from file system

--- frontend/config/runtimeConfig.ts
+++ frontend/config/runtimeConfig.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs";
 import type { NuxtConfig } from "nuxt/schema";
 import { isDevelopment } from "./shared";
 
@@ -7,8 +8,17 @@ const secureCookie = !isDevelopment;
  * Configuration for the runtimeConfig section of Nuxt config. These should be
  * set in the environment variables.
  */
+
+const getBasicAuth = () => {
+  const path = "/etc/secrets/basic-auth/secret";
+  if (fs.existsSync(path)) {
+    return fs.readFileSync(path, "utf-8").trim();
+  }
+  return process.env.NUXT_BASIC_AUTH || "";
+};
+
 export const runtimeConfig: NuxtConfig["runtimeConfig"] = {
-  basicAuth: "",
+  basicAuth: getBasicAuth(),
   auth: {
     webAuth: false,
   },
