b7f835b45f21e2193c619318da9ece72285dae41
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2026-02-02 16:05:58 +0100

    feat: wip composable for simple search route params
    RISDEV-10099

---
+++ frontend/src/composables/useSimpleSearchRouteParams.spec.ts
@@ -0,0 +1,512 @@
+import { reactive, nextTick } from "vue";
+import { DocumentKind } from "~/types";
+
+describe("useSimpleSearchRouteParams", () => {
+  beforeEach(() => {
+    vi.resetModules();
+  });
+
+  describe("document kind", () => {
+    it("restores a valid document kind from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { documentKind: "R" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { documentKind } = useSimpleSearchRouteParams();
+
+      expect(documentKind.value).toEqual(DocumentKind.CaseLaw);
+    });
+
+    it("returns 'All' when the document kind is invalid", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { documentKind: "invalid" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { documentKind } = useSimpleSearchRouteParams();
+
+      expect(documentKind.value).toEqual(DocumentKind.All);
+    });
+
+    it("returns 'All' when the query does not contain a document kind", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { documentKind } = useSimpleSearchRouteParams();
+
+      expect(documentKind.value).toEqual(DocumentKind.All);
+    });
+  });
+
+  describe("query", () => {
+    it("restores a search query from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { q: "example" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { query } = useSimpleSearchRouteParams();
+
+      expect(query.value).toBe("example");
+    });
+
+    it("returns an empty string search query by default", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { query } = useSimpleSearchRouteParams();
+
+      expect(query.value).toBe("");
+    });
+
+    it("decodes a URI encoded query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { q: encodeURIComponent("DATUM:>2021-01-01") },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { query } = useSimpleSearchRouteParams();
+
+      expect(query.value).toBe("DATUM:>2021-01-01");
+    });
+  });
+
+  describe("date filter", () => {
+    it("restores a valid date filter type from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { dateFilterType: "allTime" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.type).toBe("allTime");
+    });
+
+    it("returns 'allTime' when the date filter type is invalid", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { dateFilterType: "invalid" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.type).toBe("allTime");
+    });
+
+    it("returns the default filter type when there is no value in the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.type).toBe("allTime");
+    });
+
+    it("returns the 'from' date of the date filter", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { dateFilterFrom: "2023-01-01" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.from).toBe("2023-01-01");
+    });
+
+    it("returns undefined if the 'from' date is not in the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.from).toBeUndefined();
+    });
+
+    it("returns the 'to' date of the date filter", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { dateFilterTo: "2023-12-31" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.to).toBe("2023-12-31");
+    });
+
+    it("returns undefined if the 'to' date is not in the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter } = useSimpleSearchRouteParams();
+
+      expect(dateFilter.value.to).toBeUndefined();
+    });
+  });
+
+  describe("court", () => {
+    it("restores the court from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { court: "BGH" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { court } = useSimpleSearchRouteParams();
+
+      expect(court.value).toBe("BGH");
+    });
+
+    it("returns undefined when the court is not in the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { court } = useSimpleSearchRouteParams();
+
+      expect(court.value).toBeUndefined();
+    });
+  });
+
+  describe("sort", () => {
+    it("returns the sort param from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { sort: "relevance" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { sort } = useSimpleSearchRouteParams();
+
+      expect(sort.value).toBe("relevance");
+    });
+
+    it("returns the default sort param as a fallback", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { sort } = useSimpleSearchRouteParams();
+
+      expect(sort.value).toBe("default");
+    });
+  });
+
+  describe("items per page", () => {
+    it("returns the number of items per page from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { itemsPerPage: "100" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { itemsPerPage } = useSimpleSearchRouteParams();
+
+      expect(itemsPerPage.value).toBe("100");
+    });
+
+    it("returns the default number of items per page as a fallback", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { itemsPerPage } = useSimpleSearchRouteParams();
+
+      expect(itemsPerPage.value).toBe("50");
+    });
+  });
+
+  describe("page index", () => {
+    it("returns the page index from the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { pageIndex: "5" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { pageIndex } = useSimpleSearchRouteParams();
+
+      expect(pageIndex.value).toBe(5);
+    });
+
+    it("returns the first page if the value from the query can't be parsed", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: { pageIndex: "invalid" },
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { pageIndex } = useSimpleSearchRouteParams();
+
+      expect(pageIndex.value).toBe(0);
+    });
+
+    it("returns the first page if there is no value in the query", async () => {
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { pageIndex } = useSimpleSearchRouteParams();
+
+      expect(pageIndex.value).toBe(0);
+    });
+  });
+
+  describe("saveFilterStateToRoute", () => {
+    it("calls navigateTo with all filter parameters", async () => {
+      const navigateToMock = vi.fn();
+      const existingQuery = { existingParam: "value" };
+
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: existingQuery,
+        }),
+        navigateTo: navigateToMock,
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const {
+        query,
+        documentKind,
+        court,
+        dateFilter,
+        pageIndex,
+        sort,
+        itemsPerPage,
+        saveFilterStateToRoute,
+      } = useSimpleSearchRouteParams();
+
+      query.value = "test search";
+      documentKind.value = DocumentKind.CaseLaw;
+      court.value = "BGH";
+      dateFilter.value = {
+        type: "allTime",
+        from: "2023-01-01",
+        to: "2023-12-31",
+      };
+      pageIndex.value = 2;
+      sort.value = "relevance";
+      itemsPerPage.value = "100";
+
+      await saveFilterStateToRoute();
+
+      expect(navigateToMock).toHaveBeenCalledWith({
+        query: {
+          existingParam: "value",
+          q: "test%20search",
+          court: "BGH",
+          documentKind: "R",
+          dateFilterType: "allTime",
+          dateFilterFrom: "2023-01-01",
+          dateFilterTo: "2023-12-31",
+          pageIndex: 2,
+          sort: "relevance",
+          itemsPerPage: "100",
+        },
+      });
+    });
+
+    it("encodes the query string", async () => {
+      const navigateToMock = vi.fn();
+
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+        navigateTo: navigateToMock,
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { query, saveFilterStateToRoute } = useSimpleSearchRouteParams();
+
+      query.value = "DATUM:>2021-01-01";
+      await saveFilterStateToRoute();
+
+      expect(navigateToMock).toHaveBeenCalledWith({
+        query: expect.objectContaining({
+          q: encodeURIComponent("DATUM:>2021-01-01"),
+        }),
+      });
+    });
+
+    it("handles undefined date filter values by setting empty strings", async () => {
+      const navigateToMock = vi.fn();
+
+      vi.doMock("#app", () => ({
+        useRoute: vi.fn().mockReturnValue({
+          query: {},
+        }),
+        navigateTo: navigateToMock,
+      }));
+
+      const { useSimpleSearchRouteParams } = await import(
+        "./useSimpleSearchRouteParams"
+      );
+
+      const { dateFilter, saveFilterStateToRoute } =
+        useSimpleSearchRouteParams();
+
+      dateFilter.value = {
+        type: "allTime",
+        from: undefined,
+        to: undefined,
+      };
+
+      await saveFilterStateToRoute();
+
+      expect(navigateToMock).toHaveBeenCalledWith({
+        query: expect.objectContaining({
+          dateFilterFrom: "",
+          dateFilterTo: "",
+        }),
+      });
+    });
+  });
+
+  it("reacts to route changes", async () => {
+    const routeQuery = reactive({ query: { q: "test before" } });
+
+    vi.doMock("#app", () => ({
+      useRoute: vi.fn().mockReturnValue(routeQuery),
+    }));
+
+    const { useSimpleSearchRouteParams } = await import(
+      "./useSimpleSearchRouteParams"
+    );
+
+    const { query } = useSimpleSearchRouteParams();
+
+    expect(query.value).toEqual("test before");
+
+    routeQuery.query = { q: "test after" };
+    await nextTick();
+
+    expect(query.value).toEqual("test after");
+  });
+});
---
+++ frontend/src/composables/useSimpleSearchRouteParams.ts
@@ -0,0 +1,106 @@
+import { navigateTo, useRoute } from "#app";
+import type { LocationQueryValue } from "#vue-router";
+import { DocumentKind } from "~/types";
+import { isDocumentKind } from "~/utils/documentKind";
+import { isFilterType, type DateFilterValue } from "~/utils/search/filterType";
+import { searchParamToNumber, searchParamToString } from "~/utils/searchParams";
+
+export function useSimpleSearchRouteParams() {
+  const route = useRoute();
+
+  // General parameters -------------------------------------
+
+  // TODO: Reset document-type specific options
+  // TODO: Subtypes (e.g. `R.decision`)
+  const documentKind = ref<DocumentKind>(DocumentKind.All);
+
+  const query = ref<string>("");
+
+  // Date filter --------------------------------------------
+
+  function getInitialFilterTypeFromQuery(
+    init: LocationQueryValue | LocationQueryValue[] | undefined,
+  ): DateFilterValue["type"] {
+    if (typeof init !== "string" || !isFilterType(init)) {
+      return "allTime";
+    }
+
+    return init;
+  }
+
+  const dateFilter = ref<DateFilterValue>({ type: "allTime" });
+
+  // Court filter -------------------------------------------
+
+  const court = ref<string>();
+
+  // Sort & pagination --------------------------------------
+
+  const sort = ref<string>("default");
+
+  const itemsPerPage = ref<string>("10");
+
+  const pageIndex = ref<number>(0);
+
+  // Saving and restoring -----------------------------------
+
+  function loadFilterStateFromRoute(routeQuery = route.query) {
+    const documentKindParam = searchParamToString(routeQuery.documentKind);
+    documentKind.value =
+      documentKindParam && isDocumentKind(documentKindParam)
+        ? documentKindParam
+        : DocumentKind.All;
+
+    query.value = decodeURIComponent(searchParamToString(routeQuery.q) ?? "");
+
+    court.value = searchParamToString(routeQuery.court);
+
+    dateFilter.value = {
+      type: getInitialFilterTypeFromQuery(routeQuery.dateFilterType),
+      from: searchParamToString(routeQuery.dateFilterFrom),
+      to: searchParamToString(routeQuery.dateFilterTo),
+    };
+
+    sort.value = searchParamToString(routeQuery.sort) ?? "default";
+
+    itemsPerPage.value = searchParamToString(routeQuery.itemsPerPage) ?? "50";
+
+    pageIndex.value = searchParamToNumber(routeQuery.pageIndex, 0);
+  }
+
+  function saveFilterStateToRoute() {
+    // TODO: PostHog integration
+
+    return navigateTo({
+      query: {
+        ...route.query,
+        q: encodeURIComponent(query.value),
+        court: court.value,
+        documentKind: documentKind.value,
+        dateFilterType: dateFilter.value.type,
+        dateFilterFrom: dateFilter.value.from ?? "",
+        dateFilterTo: dateFilter.value.to ?? "",
+        pageIndex: pageIndex.value,
+        sort: sort.value,
+        itemsPerPage: itemsPerPage.value,
+      },
+    });
+  }
+
+  watch(
+    () => route.query,
+    (val) => loadFilterStateFromRoute(val),
+    { immediate: true },
+  );
+
+  return {
+    court,
+    dateFilter,
+    documentKind,
+    itemsPerPage,
+    pageIndex,
+    query,
+    saveFilterStateToRoute,
+    sort,
+  };
+}
