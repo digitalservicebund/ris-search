1ba3c85671f80d6492a847e671e7c9add803e1b4
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2026-04-02 09:27:02 +0200

    chore: clean up comments
    RISDEV-11269

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -50,18 +50,12 @@ const projects: Project[] = [
   },
 ];
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./e2e",
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: process.env.CI
     ? [
         ["line", { printSteps: true }],
@@ -69,23 +63,23 @@ export default defineConfig({
       ]
     : "html",
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: environment.baseUrl,
     screenshot: { mode: "only-on-failure", fullPage: true },
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "retain-on-first-failure",
   },
 
   expect: {
     toHaveScreenshot: {
       maxDiffPixels: 100,
+
+      // Deliberately excludes the operating system (included in the default
+      // template) so the snapshots can be used both locally as well as in the
+      // pipeline.
       pathTemplate:
         "{testDir}/__snapshots__/{testFileName}/{testName}-{projectName}{ext}",
     },
   },
 
-  /* Configure projects for major browsers */
   projects,
 });
