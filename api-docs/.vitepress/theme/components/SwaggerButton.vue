2ec885aa298439168aa1ba5710993207eb08fa84
UrsKahmann <33418462+UrsKahmann@users.noreply.github.com>
2026-04-28 07:33:05 +0200

    Fix swagger button hover styling (#1920)
    - fix button hover text color
    - give swagger link button in sidebar bottom margin so the focus state
    border is not cut off
    
    RISDEV-11297

--- api-docs/.vitepress/theme/components/SwaggerButton.vue
+++ api-docs/.vitepress/theme/components/SwaggerButton.vue
@@ -8,7 +8,7 @@ const { theme } = useData();
   <ButtonLink
     :href="theme.swaggerUiUrl"
     target="_blank"
-    class="not-prose mt-16 bg-blue-800 hover:bg-blue-600 p-16 ris-label2-bold text-white no-underline focus:text-white focus-visible:text-white"
+    class="not-prose mt-16 bg-blue-800 hover:bg-blue-600 hover:text-white p-16 ris-label2-bold text-white no-underline focus:text-white focus-visible:text-white"
   >
     Test the API in Swagger
   </ButtonLink>
