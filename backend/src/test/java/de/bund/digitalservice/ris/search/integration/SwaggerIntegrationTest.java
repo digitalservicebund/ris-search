dd023057f73d932b9c74967d9b3f0d961fd984c4
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-04-28 08:12:04 +0200

    Remove openapi.json upload to docs repo
    - adapt test that generates the openapi.json so it is now only added to the frontend
    - remove unused flag
    
    RISDEV-11176

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/SwaggerIntegrationTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/SwaggerIntegrationTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -21,12 +20,12 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
 /*
- * Advanced search functionality is disabled in this test so it doesn't show up in the generated openapi.json file.
+ * Test that generates an openapi.json by running the backend and querying the /v3/api-docs
+ * endpoint. The file is written to the frontend so it can be used there for type generation.
  */
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
     properties = {
-      "feature-flags.advanced-search=false",
       "swagger.server.url=https://testphase.rechtsinformationen.bund.de",
       "swagger.server.description=Public API (preview)"
     })
@@ -44,21 +43,17 @@ class SwaggerIntegrationTest extends ContainersIntegrationBase {
         .andDo(
             item -> {
               byte[] content = item.getResponse().getContentAsByteArray();
-              // Maintaining both the frontend /docs url and portal-docs-repo
-              List.of("./../frontend/src/public", "./out")
-                  .forEach(
-                      path -> {
-                        Path outDir = Paths.get(path);
-                        try {
-                          Files.createDirectories(outDir);
-                          File outputFile = new File(outDir.toFile(), "openapi.json");
-                          try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-                            outputStream.write(content);
-                          }
-                        } catch (IOException exception) {
-                          throw new RuntimeException(exception);
-                        }
-                      });
+              Path outDir = Paths.get("./../frontend/src/public");
+
+              try {
+                Files.createDirectories(outDir);
+                File outputFile = new File(outDir.toFile(), "openapi.json");
+                try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+                  outputStream.write(content);
+                }
+              } catch (IOException exception) {
+                throw new RuntimeException(exception);
+              }
             })
         .andExpect(
             jsonPath(
