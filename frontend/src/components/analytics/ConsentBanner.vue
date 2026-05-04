ed944dde42bb4ff5cfb2b3a4e6285931b3009276
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-05-04 13:58:41 +0200

    fix: remove redundant labelling
    RISDEV-11532

--- frontend/src/components/analytics/ConsentBanner.vue
+++ frontend/src/components/analytics/ConsentBanner.vue
@@ -12,17 +12,19 @@ if (import.meta.server) {
 const handleSetTracking = async (value: boolean) => {
   await setTracking(value);
 };
+
+const headingId = useId();
 </script>
 
 <template>
   <section
     v-if="isBannerVisible"
     class="ris-body2-regular lg:ris-body1-regular flex flex-col gap-24 bg-blue-200 px-8 py-16 lg:p-24"
-    aria-label="Cookie-Einstellungen akzeptieren oder ablehnen"
+    :aria-labelledby="headingId"
     data-testid="cookie-banner"
   >
     <div class="container">
-      <h2 class="ris-heading3-bold text-xl lg:text-2xl">
+      <h2 :id="headingId" class="ris-heading3-bold text-xl lg:text-2xl">
         Cookie-Einstellungen akzeptieren oder ablehnen
       </h2>
 
