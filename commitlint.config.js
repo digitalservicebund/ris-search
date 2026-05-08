17d4de25d0638587f50feadb936102b12e3ea0a9
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-05-08 07:03:28 +0200

    RISDEV-0000 remove commit message linting
    Because of our practice of committing by merging PRs and squashing them,
    individual local commit messages (where the linting applies) don't
    matter much anyway, so we don't need the linting.

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  rules: {
-    "body-case": [2, "always", "sentence-case"],
-    "body-leading-blank": [2, "always"],
-    "header-full-stop": [2, "never", "."],
-    "header-max-length": [2, "always", 60],
-  },
-};
