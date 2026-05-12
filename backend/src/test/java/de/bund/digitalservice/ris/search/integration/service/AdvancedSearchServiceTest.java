b5050481cbc0f4bc9d4b669da1859013b0619db2
andrew.cenkner <andrew.cenkner@digitalservice.bund.de>
2026-05-12 07:33:47 +0200

    RISDEV-10554
    Add tests

--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/service/AdvancedSearchServiceTest.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/service/AdvancedSearchServiceTest.java
@@ -2,14 +2,16 @@ package de.bund.digitalservice.ris.search.integration.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import de.bund.digitalservice.ris.search.exception.CustomValidationException;
 import de.bund.digitalservice.ris.search.integration.config.ContainersIntegrationBase;
-import de.bund.digitalservice.ris.search.models.opensearch.AbstractSearchEntity;
 import de.bund.digitalservice.ris.search.models.opensearch.AdministrativeDirective;
 import de.bund.digitalservice.ris.search.models.opensearch.CaseLawDocumentationUnit;
 import de.bund.digitalservice.ris.search.models.opensearch.Literature;
 import de.bund.digitalservice.ris.search.models.opensearch.Norm;
 import de.bund.digitalservice.ris.search.service.AdvancedSearchService;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -29,34 +32,41 @@ class AdvancedSearchServiceTest extends ContainersIntegrationBase {
   @Test
   @DisplayName("Advanced search for all documents works")
   void advancedSearchForAllDocumentsWorks() {
-    SearchHits<AbstractSearchEntity> searchHits;
-
-    searchHits =
-        advancedSearchService.searchAll("BFRE000087655", Pageable.unpaged()).getSearchHits();
-    assertThat(searchHits).hasSize(1);
-    CaseLawDocumentationUnit caseLaw =
-        (CaseLawDocumentationUnit) searchHits.getSearchHit(0).getContent();
-    assertThat(caseLaw.documentNumber()).isEqualTo("BFRE000087655");
-
-    searchHits =
-        advancedSearchService.searchAll("KALU000000001", Pageable.unpaged()).getSearchHits();
-    assertThat(searchHits).hasSize(1);
-    Literature literature = (Literature) searchHits.getSearchHit(0).getContent();
-    assertThat(literature.documentNumber()).isEqualTo("KALU000000001");
-
-    searchHits =
+    checkSingleSearchHitsEquivalence(
+        advancedSearchService.searchAll("BFRE000087655", Pageable.unpaged()).getSearchHits(),
+        advancedSearchService.searchCaseLaw("BFRE000087655", Pageable.unpaged()).getSearchHits());
+    checkSingleSearchHitsEquivalence(
+        advancedSearchService.searchAll("KALU000000001", Pageable.unpaged()).getSearchHits(),
         advancedSearchService
-            .searchAll("\"eli/bund/bgbl-1/1000/test/2000-10-06/2/deu\"", Pageable.unpaged())
-            .getSearchHits();
-    assertThat(searchHits).hasSize(1);
-    Norm norm = (Norm) searchHits.getSearchHit(0).getContent();
-    assertThat(norm.getId()).isEqualTo("eli/bund/bgbl-1/1000/test/2000-10-06/2/deu");
+            .searchLiterature("KALU000000001", Pageable.unpaged())
+            .getSearchHits());
+    checkSingleSearchHitsEquivalence(
+        advancedSearchService
+            .searchAll("expression_eli:2000-10-06", Pageable.unpaged())
+            .getSearchHits(),
+        advancedSearchService
+            .searchNorm("expression_eli:2000-10-06", Pageable.unpaged())
+            .getSearchHits());
+    checkSingleSearchHitsEquivalence(
+        advancedSearchService.searchAll("KSNR0000", Pageable.unpaged()).getSearchHits(),
+        advancedSearchService
+            .searchAdministrativeDirective("KSNR0000", Pageable.unpaged())
+            .getSearchHits());
+  }
 
-    searchHits = advancedSearchService.searchAll("KSNR0000", Pageable.unpaged()).getSearchHits();
-    assertThat(searchHits).hasSize(1);
-    AdministrativeDirective administrativeDirective =
-        (AdministrativeDirective) searchHits.getSearchHit(0).getContent();
-    assertThat(administrativeDirective.documentNumber()).isEqualTo("KSNR0000");
+  private <T, V> void checkSingleSearchHitsEquivalence(SearchHits<T> hits1, SearchHits<V> hits2) {
+    assertThat(hits1.getTotalHits()).isEqualTo(hits2.getTotalHits()).isEqualTo(1);
+    SearchHit<T> hit1 = hits1.getSearchHit(0);
+    SearchHit<V> hit2 = hits2.getSearchHit(0);
+    assertThat(convertMapKeys(hit1.getHighlightFields()))
+        .containsExactlyInAnyOrderEntriesOf(convertMapKeys(hit2.getHighlightFields()));
+    assertThat(hit1.getContent()).isEqualTo(hit2.getContent());
+  }
+
+  private <T> Map<String, T> convertMapKeys(Map<String, T> input) {
+    return input.entrySet().stream()
+        .collect(
+            Collectors.toMap(e -> e.getKey().replace("_", "").toLowerCase(), Map.Entry::getValue));
   }
 
   @Test
@@ -95,15 +105,95 @@ class AdvancedSearchServiceTest extends ContainersIntegrationBase {
   }
 
   @Test
-  @DisplayName("Advanced search for norm works")
-  void advancedSearchForNormWorks() throws CustomValidationException {
-
+  @DisplayName("norm advanced search works")
+  void normAdvancedSearchWorks() {
     var searchHits =
         advancedSearchService
-            .searchNorm("\"eli/bund/bgbl-1/1000/test/2000-10-06/2/deu\"", Pageable.unpaged())
+            .searchNorm("expression_eli:2000-10-06", Pageable.unpaged())
             .getSearchHits();
+
+    // check that the metadata is returned
     assertThat(searchHits).hasSize(1);
-    Norm caseLaw = searchHits.getSearchHit(0).getContent();
-    assertThat(caseLaw.getId()).isEqualTo("eli/bund/bgbl-1/1000/test/2000-10-06/2/deu");
+    SearchHit<Norm> searchHit = searchHits.getSearchHit(0);
+    assertThat(searchHit.getId()).isEqualTo("eli/bund/bgbl-1/1000/test/2000-10-06/2/deu");
+    Norm norm = searchHit.getContent();
+    assertThat(norm.getOfficialShortTitle()).isEqualTo("TestG1");
+    // check that the content is NOT returned
+    assertThat(norm.getArticleNames()).isNull();
+  }
+
+  @Test
+  @DisplayName("norm advanced search with highlights works")
+  void normAdvancedSearchWithHighlightsWorks() {
+
+    var searchHit =
+        advancedSearchService
+            .searchNorm("expression_eli:2000-10-06 AND test OR example", Pageable.unpaged())
+            .getSearchHits()
+            .getSearchHit(0);
+
+    // check that the title was highlighted
+    Map<String, String> nonArticleHighlights = getFieldMatches(searchHit);
+    assertThat(nonArticleHighlights)
+        .containsOnly(Map.entry("officialTitle", "<mark>Test</mark> Gesetz"));
+
+    // check that the article highlights work
+    List<Pair<String, String>> articleHighlights =
+        searchHit
+            .getInnerHits()
+            .get("top_three_articles")
+            .get()
+            .map(this::getArticleTextMatch)
+            .toList();
+    assertThat(articleHighlights)
+        .containsExactly(
+            Pair.of("§ 1 <mark>Example</mark> article", "<mark>example</mark> text 1"),
+            Pair.of("§ 2 <mark>Example</mark> article", "<mark>example</mark> text 2"));
+  }
+
+  @Test
+  @DisplayName("norm advanced search without highlights works")
+  void normAdvancedSearchWithoutHighlightsWorks() {
+
+    var searchHit =
+        advancedSearchService
+            .searchNorm("expression_eli:2000-10-06", Pageable.unpaged())
+            .getSearchHits()
+            .getSearchHit(0);
+
+    // Check that the title was highlighted
+    Map<String, String> nonArticleHighlights = getFieldMatches(searchHit);
+    assertThat(nonArticleHighlights).containsOnly(Map.entry("officialTitle", "Test Gesetz"));
+
+    // check the article highlights work
+    List<Pair<String, String>> articleHighlights =
+        searchHit
+            .getInnerHits()
+            .get("top_three_articles")
+            .get()
+            .map(this::getArticleTextMatch)
+            .toList();
+    assertThat(articleHighlights)
+        .containsExactly(
+            Pair.of("§ 1 Example article", "example text 1"),
+            Pair.of("§ 2 Example article", "example text 2"));
+  }
+
+  private Pair<String, String> getArticleTextMatch(SearchHit<?> searchHit) {
+    Map<String, String> fieldMatches = getFieldMatches(searchHit);
+    assertThat(fieldMatches.keySet()).containsExactlyInAnyOrder("name", "text");
+    return Pair.of(fieldMatches.get("name"), fieldMatches.get("text"));
+  }
+
+  // Convert the SearchHit highlights to a simple map since we can assume there is one match per
+  // field
+  private Map<String, String> getFieldMatches(SearchHit<?> searchHit) {
+    Map<String, List<String>> textMatches = searchHit.getHighlightFields();
+    for (List<String> textMatch : textMatches.values()) {
+      // we set numOfFragments = 1 so we expect exactly one match per field
+      assertThat(textMatch.size() == 1);
+    }
+    return textMatches.entrySet().stream()
+        .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().getFirst()));
   }
 }
