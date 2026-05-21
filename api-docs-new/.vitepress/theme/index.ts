d7e616cce1903bef053aa701f22f7ebf14867fef
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-05-21 09:07:52 +0200

    Add "this is a trial" banner

--- api-docs-new/.vitepress/theme/index.ts
+++ api-docs-new/.vitepress/theme/index.ts
@@ -1,9 +1,13 @@
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
 import FeedbackForm from "./components/FeedbackForm.vue";
+import Layout from "./Layout.vue";
+// @ts-ignore
+import "./custom.css";
 
 export default {
   extends: DefaultTheme,
+  Layout,
   enhanceApp({ app }) {
     app.component("FeedbackForm", FeedbackForm);
   },
