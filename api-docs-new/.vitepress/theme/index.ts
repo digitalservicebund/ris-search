999ab9e3793c830a9a42b3cc7297f2a2c3cdbdb0
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-05-27 14:08:50 +0200

    Remove banner
    RISDEV-11501

--- api-docs-new/.vitepress/theme/index.ts
+++ api-docs-new/.vitepress/theme/index.ts
@@ -2,8 +2,6 @@ import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
 import FeedbackForm from "./components/FeedbackForm.vue";
 import Layout from "./Layout.vue";
-// @ts-ignore
-import "./custom.css";
 
 export default {
   extends: DefaultTheme,
