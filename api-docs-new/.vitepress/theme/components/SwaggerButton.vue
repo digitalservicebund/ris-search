3388da6a2fac4f0b4cc09ca6c26e39dd347ad404
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-05-28 16:26:51 +0200

    Replace action button with swagger button

---
+++ api-docs-new/.vitepress/theme/components/SwaggerButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <a
+    class="RisButton"
+    href="https://docs.rechtsinformationen.bund.de/swagger-ui/index.html"
+    target="_blank"
+    rel="noreferrer"
+  >
+    Test the API in Swagger
+  </a>
+</template>
