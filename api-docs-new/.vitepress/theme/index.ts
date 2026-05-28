95bcc72059f0bebe15d06930501d47a3dbd68e49
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-05-28 16:43:06 +0200

    Replace prettier with oxfmt

--- api-docs-new/.vitepress/theme/index.ts
+++ api-docs-new/.vitepress/theme/index.ts
@@ -1,8 +1,8 @@
-import DefaultTheme from "vitepress/theme";
 import type { Theme } from "vitepress";
-import Layout from "./Layout.vue";
-import SwaggerButton from "./components/SwaggerButton.vue";
+import DefaultTheme from "vitepress/theme";
 import Footer from "./components/Footer.vue";
+import SwaggerButton from "./components/SwaggerButton.vue";
+import Layout from "./Layout.vue";
 import "./custom.css";
 
 export default {
