329f86c94b0f76c058927d0a5a27444feda3d8d2
Eric Klemm <eric.klemm@digitalservice.bund.de>
2026-06-05 14:03:21 +0200

    log client abort exception and return gracefully

--- backend/src/main/java/de/bund/digitalservice/ris/search/controller/api/ControllerExceptionHandler.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/controller/api/ControllerExceptionHandler.java
@@ -11,10 +11,13 @@ import jakarta.validation.Path;
 import java.nio.file.AccessDeniedException;
 import java.util.List;
 import java.util.Objects;
+import org.apache.catalina.connector.ClientAbortException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -306,5 +309,38 @@ public class ControllerExceptionHandler {
     CustomErrorResponse errorResponse =
         CustomErrorResponse.builder().errors(List.of(error)).build();
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+  }
+
+  /**
+   * Checks if a HttpMessageNotWritableException is wrapping a ClientAbortException and delegates to
+   * that handler. This might happen if a client aborts a request during json serialization.
+   *
+   * @param exception HttpMessageNotWritableException
+   */
+  @ExceptionHandler({HttpMessageNotWritableException.class})
+  public void handleHttpMessageNotWritableException(HttpMessageNotWritableException exception)
+      throws HttpMessageNotWritableException {
+    Throwable rootCause = NestedExceptionUtils.getMostSpecificCause(exception);
+
+    if (rootCause instanceof ClientAbortException clientabortexception) {
+      handleClientAbortException(clientabortexception);
+    }
+    throw exception;
+  }
+
+  /**
+   * Logs ClientAbortExceptions with log level warn. Does not return a ResponseEntity since the
+   * connection is lost already.
+   *
+   * @param exception ClientAbortException
+   */
+  @ExceptionHandler({ClientAbortException.class})
+  public void handleClientAbortException(ClientAbortException exception) {
+    // Get the absolute root cause of the exception chain
+    Throwable rootCause = NestedExceptionUtils.getMostSpecificCause(exception);
+
+    if (rootCause instanceof ClientAbortException) {
+      logger.warn("connection closed by client", rootCause);
+    }
   }
 }
--- backend/src/test/java/de/bund/digitalservice/ris/search/integration/config/TestMockEndpointsController.java
+++ backend/src/test/java/de/bund/digitalservice/ris/search/integration/config/TestMockEndpointsController.java
@@ -1,8 +1,10 @@
 package de.bund.digitalservice.ris.search.integration.config;
 
 import java.nio.file.AccessDeniedException;
+import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,27 @@ public class TestMockEndpointsController {
   @GetMapping("/internalError")
   public ResponseEntity<String> internalErrorResponse() throws Exception {
     throw new Exception("Unknown error");
+  }
+
+  @GetMapping("/clientAbort")
+  public ClientAbortedResponse triggerNestedAbort() {
+    return new ClientAbortedResponse();
+  }
+
+  @GetMapping("throwHttpMessageNotWritableException")
+  public void throwHttpMessageNotWritableException() {
+    throw new HttpMessageNotWritableException("message not writable");
+  }
+
+  /** A DTO designed to throw a ClientAbortException during JSON serialization */
+  public static class ClientAbortedResponse {
+    /**
+     * When Jackson invokes this getter, it simulates a broken pipe mid-stream
+     *
+     * @return String property that is never actually being returned
+     */
+    public String getBrokenStreamProperty() throws ClientAbortException {
+      throw new ClientAbortException("java.io.IOException: Broken pipe");
+    }
   }
 }
