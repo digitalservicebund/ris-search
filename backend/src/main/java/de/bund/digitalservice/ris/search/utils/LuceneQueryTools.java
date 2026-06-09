d291ca98b43e9270ed5c7f8939b6e34192a8bd32
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-06-09 11:20:18 +0200

    Fix formatting

--- backend/src/main/java/de/bund/digitalservice/ris/search/utils/LuceneQueryTools.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/utils/LuceneQueryTools.java
@@ -62,8 +62,7 @@ public class LuceneQueryTools {
    * @return A new query string with all terms OR-combined
    * @throws CustomValidationException in case a queryString is invalid
    */
-  public static String joinAllTermsWithOr(String queryString)
-      throws CustomValidationException {
+  public static String joinAllTermsWithOr(String queryString) throws CustomValidationException {
     if (queryString == null || queryString.isBlank()) {
       return "";
     }
