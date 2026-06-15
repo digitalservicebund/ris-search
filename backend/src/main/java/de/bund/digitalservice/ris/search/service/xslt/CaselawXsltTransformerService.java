aa46d7df4429061698d2bf08c6617270212dca67
Magdalena Noffke <magdalena.noffke@digitalservice.bund.de>
2026-06-15 19:27:09 +0200

    Use transformer from library in service

--- backend/src/main/java/de/bund/digitalservice/ris/search/service/xslt/CaselawXsltTransformerService.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/service/xslt/CaselawXsltTransformerService.java
@@ -1,21 +1,11 @@
 package de.bund.digitalservice.ris.search.service.xslt;
 
-import java.util.Map;
+import de.bund.digitalservice.ris.html.service.xslt.CaselawXsltTransformer;
 import org.springframework.stereotype.Service;
 
 /** Service for transforming LegalDocML case law documents to HTML using XSLT. */
 @Service
-public class CaselawXsltTransformerService extends XsltTransformer {
-
-  @Override
-  String getXsltBasePath() {
-    return "XSLT/html/";
-  }
-
-  @Override
-  String getXsltFilename() {
-    return "case-law.xslt";
-  }
+public class CaselawXsltTransformerService {
 
   /**
    * Key for the resource path parameter passed to the XSLT transformer.
@@ -25,7 +15,7 @@ public class CaselawXsltTransformerService extends XsltTransformer {
    * @return the transformed HTML as a String
    */
   public String transformCaseLaw(byte[] source, String resourcesBasePath) {
-    Map<String, String> parameters = Map.of(RESOURCE_PATH_KEY, resourcesBasePath);
-    return transformLegalDocMlFromBytes(source, parameters);
+    CaselawXsltTransformer caselawXsltTransformer = new CaselawXsltTransformer();
+    return caselawXsltTransformer.transformCaseLaw(source, resourcesBasePath);
   }
 }
