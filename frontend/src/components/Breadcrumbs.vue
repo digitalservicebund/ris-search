d6c76b68bff0732ca2c5be7e7280f9cc4d6db77c
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-06-17 15:15:24 +0200

    feat: add new typo classes

--- frontend/src/components/Breadcrumbs.vue
+++ frontend/src/components/Breadcrumbs.vue
@@ -129,14 +129,14 @@ const drawerId = useId();
           v-if="i.route && i !== itemsWithHome.at(-1)"
           :to="i.route"
           @click="drawerVisible = false"
-          class="typo-body-link link-hover flex py-12"
+          class="typo-link-regular link-hover flex py-12"
         >
           {{ i.extendedLabel ?? i.label }}
         </NuxtLink>
 
         <span
           v-else
-          class="typo-body flex cursor-not-allowed py-12 text-gray-900"
+          class="typo-body-regular flex cursor-not-allowed py-12 text-gray-900"
         >
           {{ i.extendedLabel ?? i.label }}
         </span>
