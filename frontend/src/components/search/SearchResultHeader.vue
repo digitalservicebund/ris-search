9071b384ffffd04fb1a54955ce74fb9fff630299
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-06-23 15:55:56 +0200

    fix: use label2 variant for search result metadata

--- frontend/src/components/search/SearchResultHeader.vue
+++ frontend/src/components/search/SearchResultHeader.vue
@@ -16,7 +16,7 @@ const itemsWithContent = computed(() => items.filter((i) => !!i.value));
 <template>
   <div class="flex gap-8">
     <p
-      class="typo-label1-regular content-center space-x-8 wrap-anywhere hyphens-auto"
+      class="typo-label2-regular content-center space-x-8 wrap-anywhere hyphens-auto"
     >
       <span v-if="icon" class="inline-flex h-lh items-center align-text-bottom">
         <component :is="icon" class="inline-block h-16 w-16 text-gray-900" />
