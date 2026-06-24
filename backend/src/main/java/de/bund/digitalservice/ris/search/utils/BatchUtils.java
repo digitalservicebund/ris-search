0d7a2e5e4c678cea36f3e0387ef52bd9cfecca39
andrew.cenkner <andrew.cenkner@digitalservice.bund.de>
2026-06-24 08:24:57 +0200

    RISDEV-11833
    increase norms index performance
    *Add javadoc

--- backend/src/main/java/de/bund/digitalservice/ris/search/utils/BatchUtils.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/utils/BatchUtils.java
@@ -13,7 +13,8 @@ public class BatchUtils {
    * Processes a function over a list splitting the calls into batches.
    *
    * @param list The list to be processed.
-   * @param batchSize The size of the batching
+   * @param <T> the type of elements in the list.
+   * @param batchSize The size of the batching.
    * @param batchProcessor The method to be applied while processing.
    */
   public static <T> void processInBatches(
