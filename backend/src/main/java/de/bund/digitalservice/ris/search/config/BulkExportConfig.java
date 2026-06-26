cc9c7e892c146c29d2f493d3267f1ab81c3fde07
andrew-cenkner-digitalservice <andrew.cenkner@digitalservice.bund.de>
2026-06-26 13:51:34 +0200

    RISDEV-11663 (#2159)
    Cronjob for document kind ZIP
    *Add cronjob for other doc kinds

--- backend/src/main/java/de/bund/digitalservice/ris/search/config/BulkExportConfig.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/config/BulkExportConfig.java
@@ -1,7 +1,9 @@
 package de.bund.digitalservice.ris.search.config;
 
 import de.bund.digitalservice.ris.search.models.DocumentKind;
+import de.bund.digitalservice.ris.search.repository.objectstorage.AdministrativeDirectiveBucket;
 import de.bund.digitalservice.ris.search.repository.objectstorage.CaseLawBucket;
+import de.bund.digitalservice.ris.search.repository.objectstorage.LiteratureBucket;
 import de.bund.digitalservice.ris.search.repository.objectstorage.NormsBucket;
 import de.bund.digitalservice.ris.search.repository.objectstorage.PortalBucket;
 import de.bund.digitalservice.ris.search.repository.objectstorage.PublicFilesBucket;
@@ -40,6 +42,34 @@ public class BulkExportConfig {
   }
 
   /**
+   * @param source sourceBucket to create the document snapshot from
+   * @param target targetBucket to create the archive in
+   * @return BulkExportService
+   */
+  @Bean
+  public BulkExportService adminBulkExportService(
+      AdministrativeDirectiveBucket source, PublicFilesBucket target) {
+    return new BulkExportService(
+        source,
+        target,
+        DocumentKind.ADMINISTRATIVE_DIRECTIVE.getBulkZipPath(),
+        "",
+        filterChangelogFiles());
+  }
+
+  /**
+   * @param source sourceBucket to create the document snapshot from
+   * @param target targetBucket to create the archive in
+   * @return BulkExportService
+   */
+  @Bean
+  public BulkExportService literatureBulkExportService(
+      LiteratureBucket source, PublicFilesBucket target) {
+    return new BulkExportService(
+        source, target, DocumentKind.LITERATURE.getBulkZipPath(), "", filterChangelogFiles());
+  }
+
+  /**
    * @param normsBulkExportService the export service for norms
    * @param changelogService the changelogService used to observe file changes
    * @param portalBucket portalBucket to store the job state
@@ -58,7 +88,7 @@ public class BulkExportConfig {
   }
 
   /**
-   * @param caseLawBulkExportService the export service for norms
+   * @param caseLawBulkExportService the export service for case law
    * @param changelogService the changelogService used to observe file changes
    * @param portalBucket portalBucket to store the job state
    * @return BulkExportJob
@@ -72,6 +102,42 @@ public class BulkExportConfig {
         caseLawBulkExportService,
         portalBucket,
         DocumentKind.CASE_LAW.getBulkZipPath(),
+        changelogService);
+  }
+
+  /**
+   * @param adminBulkExportService the export service for admin
+   * @param changelogService the changelogService used to observe file changes
+   * @param portalBucket portalBucket to store the job state
+   * @return BulkExportJob
+   */
+  @Bean
+  public BulkExportJob adminBulkExport(
+      BulkExportService adminBulkExportService,
+      ChangelogService<AdministrativeDirectiveBucket> changelogService,
+      PortalBucket portalBucket) {
+    return new BulkExportJob(
+        adminBulkExportService,
+        portalBucket,
+        DocumentKind.ADMINISTRATIVE_DIRECTIVE.getBulkZipPath(),
+        changelogService);
+  }
+
+  /**
+   * @param literatureBulkExportService the export service for literature
+   * @param changelogService the changelogService used to observe file changes
+   * @param portalBucket portalBucket to store the job state
+   * @return BulkExportJob
+   */
+  @Bean
+  public BulkExportJob literatureBulkExport(
+      BulkExportService literatureBulkExportService,
+      ChangelogService<LiteratureBucket> changelogService,
+      PortalBucket portalBucket) {
+    return new BulkExportJob(
+        literatureBulkExportService,
+        portalBucket,
+        DocumentKind.LITERATURE.getBulkZipPath(),
         changelogService);
   }
 
