68c0e63b91f1a2329f774137a066b6d77eaf3313
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-07-02 10:37:21 +0200

    Add test data builder conclusion support

---
+++ backend/src/test/java/de/bund/digitalservice/ris/builder/models/Conclusions.java
@@ -0,0 +1,35 @@
+package de.bund.digitalservice.ris.builder.models;
+
+import de.bund.digitalservice.ris.builder.NormTestDataBuilder;
+import de.bund.digitalservice.ris.builder.models.common.AknP;
+import de.bund.digitalservice.ris.builder.models.common.BaseElement;
+import de.bund.digitalservice.ris.builder.models.preamble.Formula;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Conclusions extends BaseElement {
+
+  @Builder.Default
+  @XmlAttribute(name = "eId")
+  private String eId = "schluss-n1";
+
+  @XmlElement(name = "formula", namespace = NormTestDataBuilder.AKN_NS)
+  private Formula formula;
+
+  public static Conclusions withText(String text) {
+    return Conclusions.builder()
+        .formula(
+            Formula.builder()
+                .eId("schluss-n1_formel-n1")
+                .refersTo("schlussformel")
+                .paragraph(AknP.withText(text))
+                .build())
+        .build();
+  }
+}
