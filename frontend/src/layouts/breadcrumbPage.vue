c8330d0b814d4628aeb1aa68c469d6f372d5897c
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2026-07-06 10:05:26 +0200

    fix: inconsistent breadcrumb position when action menu exists

--- frontend/src/layouts/breadcrumbPage.vue
+++ frontend/src/layouts/breadcrumbPage.vue
@@ -8,7 +8,7 @@ import DefaultLayout from "./default.vue";
     ensure the outline isn't cropped) -->
     <div class="mt-8 md:mt-16">
       <div
-        class="content-wrapper mb-16 has-data-[breadcrumbs-adjust=actionmenu]:-mt-4 has-data-[breadcrumbs-adjust=actionmenu]:mb-12 md:mb-24 md:has-data-[breadcrumbs-adjust=actionmenu]:mb-20"
+        class="content-wrapper mb-16 md:mb-24 md:has-data-[breadcrumbs-adjust=actionmenu]:-mt-4 md:has-data-[breadcrumbs-adjust=actionmenu]:mb-20"
       >
         <slot name="breadcrumb" />
       </div>
