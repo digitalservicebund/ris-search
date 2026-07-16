4857ea6bb25a003ebca82eca7998e62a86878897
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-07-16 11:42:34 +0200

    Add norm abbreviation route
    - add a page route that allows to open a norm directly via an abbreviation
    
    RISDEV-7898

---
+++ frontend/src/pages/gesetze/[abbreviation]/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type {
+  JSONLDList,
+  LegislationExpression,
+  SearchResult,
+} from "~/types/api";
+
+definePageMeta({
+  middleware: async (from) => {
+    const { data, error } = await useRisBackend<
+      JSONLDList<SearchResult<LegislationExpression>>
+    >("/v1/legislation", {
+      query: {
+        abbreviation: from.params.abbreviation,
+        mostRelevantOn: getCurrentDateInGermanyFormatted(),
+      },
+    });
+
+    const expressionEli = data.value?.member?.[0]?.item?.legislationIdentifier;
+    if (expressionEli && data.value?.member?.length === 1) {
+      return `/norms/${expressionEli}`;
+    }
+
+    throw createError(error.value ?? { status: 404 });
+  },
+});
+</script>
