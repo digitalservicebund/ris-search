ed796dd5f9062ba2bda112dae208faef72bee40e
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-07-21 16:44:58 +0200

    Fix style

--- api-docs/.vitepress/theme/index.ts
+++ api-docs/.vitepress/theme/index.ts
@@ -3,7 +3,7 @@ import DefaultTheme from "vitepress/theme-without-fonts";
 import SwaggerButton from "./components/SwaggerButton.vue";
 import Layout from "./Layout.vue";
 import "./custom.css";
-import "./fonts.css"
+import "./fonts.css";
 
 export default {
   extends: DefaultTheme,
