1d59031fedf9fde9692ba6ffd8d7e6b0724501d7
UrsKahmann <urs.kahmann@digitalservice.bund.de>
2026-08-18 08:46:00 +0200

    Remove div, fix vertical spacing
    RISDEV-12247

--- frontend/src/components/search/SearchResultHeader.vue
+++ frontend/src/components/search/SearchResultHeader.vue
@@ -29,38 +29,38 @@ const itemsWithContent = computed(() => items.filter((i) => !!i.value));
 </script>
 
 <template>
-  <div class="flex items-center gap-8">
-    <div>
-      <p class="typo-label2-regular content-center space-x-12 hyphens-auto">
-        <span v-if="documentType" class="font-bold!" :id="documentType.id">{{
-          documentType.value
+  <div>
+    <p
+      class="typo-label2-regular flex flex-wrap items-baseline gap-x-12 gap-y-8 hyphens-auto"
+    >
+      <span v-if="documentType" class="font-bold!" :id="documentType.id">{{
+        documentType.value
+      }}</span>
+      <template v-for="item in itemsWithContent" :key="item.value">
+        <span v-if="item.type === 'text' && !item.isMarkup" :id="item.id">{{
+          item.value
         }}</span>
-        <template v-for="item in itemsWithContent" :key="item.value">
-          <span v-if="item.type === 'text' && !item.isMarkup" :id="item.id">{{
-            item.value
-          }}</span>
-          <span
-            v-if="item.type === 'text' && item.isMarkup"
-            :id="item.id"
-            v-html="item.value"
-          />
-          <UiBadge
-            v-if="item.type === 'badge'"
-            :is-markup="item.isMarkup"
-            :class="item.class"
-            :variant="'medium'"
-            :label="item.value"
-            :color="item.color"
-            :id="item.id"
-          />
-        </template>
-      </p>
-      <p
-        v-if="secondaryItem?.value"
-        class="typo-label1-regular mt-8 hyphens-auto"
-      >
-        <span>{{ secondaryItem.value }}</span>
-      </p>
-    </div>
+        <span
+          v-if="item.type === 'text' && item.isMarkup"
+          :id="item.id"
+          v-html="item.value"
+        />
+        <UiBadge
+          v-if="item.type === 'badge'"
+          :is-markup="item.isMarkup"
+          :class="item.class"
+          :variant="'medium'"
+          :label="item.value"
+          :color="item.color"
+          :id="item.id"
+        />
+      </template>
+    </p>
+    <p
+      v-if="secondaryItem?.value"
+      class="typo-label1-regular mt-8 hyphens-auto"
+    >
+      <span>{{ secondaryItem.value }}</span>
+    </p>
   </div>
 </template>
