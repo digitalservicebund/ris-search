c24fa8ca97f0c3fa3a6ba05bfab64ab156345f53
ekl176 <eric.klemm@digitalservice.bund.de>
2026-08-19 13:53:13 +0200

    use bouncycastle bom to set dependency versions (#2411)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -86,10 +86,7 @@ dependencies {
     implementation(libs.jackson.databind)
 
     // CVE-2026-5588
-    implementation(libs.bouncycastle.bcpkix)
-
-    // CVE-2026-0636
-    implementation(libs.bouncycastle.bcprov)
+    implementation(platform(libs.bouncycastle.bom))
 
     // CVE-2026-55831
     implementation(libs.netty.codec.http)
