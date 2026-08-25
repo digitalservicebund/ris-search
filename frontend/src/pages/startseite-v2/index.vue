8e9a2bd5946b7c62474ee1c4dafe15897b45811d
Andreas Philippi <andreas.philippi@digitalservice.bund.de>
2026-08-25 10:11:50 +0200

    feat: add dummy v2 landing page

---
+++ frontend/src/pages/startseite-v2/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+definePageMeta({
+  skipLinks: [
+    { label: "Zum Inhalt", to: "#main" },
+    { label: "Zum Fußbereich", to: "#footer" },
+  ],
+  middleware: () => {
+    // For some reason our private feature flag composable doesn't work in this
+    // context, falling back to the runtime config directly instead
+    const config = useRuntimeConfig();
+    if (!config.public.privateFeaturesEnabled)
+      return navigateTo({ name: "index" });
+  },
+});
+
+useSeo({
+  title: "Schneller und direkter Zugang zu Rechtsinformationen",
+  description:
+    "Nutzen Sie das neue Rechtsinformationsportal des Bundes – Gesetze, Verordnungen und Urteile auf einen Blick.",
+  ogTitle: "Rechtsinformationen des Bundes",
+});
+</script>
+
+<template>
+  <div class="content-wrapper my-24">🚧 Under construction</div>
+</template>
+
+<style scoped>
+@reference "~/assets/main.css";
+</style>
