20f2eeec5840f6a7cb54a932aa87be8d079ca3b8
ekl176 <eric.klemm@digitalservice.bund.de>
2026-09-17 08:52:27 +0200

    RISDEV-12648 allow null type field in Article os model (#2529)

--- backend/src/main/java/de/bund/digitalservice/ris/search/models/opensearch/Article.java
+++ backend/src/main/java/de/bund/digitalservice/ris/search/models/opensearch/Article.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
@@ -77,7 +76,6 @@ public final class Article implements AbstractSearchEntity {
   @Field(name = Fields.DOCUMENT_NUMBER)
   private String documentNumber;
 
-  @NonNull
   @Field(name = Fields.TYPE)
   private LegislationPartType type;
 
